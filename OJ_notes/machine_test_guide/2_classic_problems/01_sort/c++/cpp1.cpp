602415c3a9bdc5113ad99202219ad63e6d6f2dc5
LeoSirius <851958789@qq.com>
2019-05-12 22:24:35 +0800

    add

---
+++ OJ_notes/machine_test_guide/2_classic_problems/01_sort/c++/cpp1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    int buff[100];
+    while(scanf("%d", &n) != EOF){
+        for(int i = 0; i < n; i++){
+            scanf("%d", &buff[i]);
+        }
+        sort(buff, buff+n);
+        for(int i = 0; i < n; i++){
+            printf("%d ", buff[i]);
+        }
+        printf("\n");
+    }
+}
