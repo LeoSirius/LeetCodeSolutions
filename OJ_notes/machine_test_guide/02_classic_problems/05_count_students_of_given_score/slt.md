56ab672e33ce62cf5f465486820774c1496ad278
LeoSirius <is.li.xiaoyu@qq.com>
2019-06-22 21:52:15 +0800

    add

---
+++ OJ_notes/machine_test_guide/02_classic_problems/05_count_students_of_given_score/slt.md
@@ -0,0 +1,25 @@
+## 思路1 利用hash的思路
+
+成绩分数给定在0到100之间。我们利用一个数组，索引表示成绩，对应的值表示那个成绩的学生的个数。
+
+```cpp
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    while(scanf("%d", &n) != EOF && n != 0){
+        int *hashing = new int[101]{0};  // 这样会初始化为0
+        for(int i = 0; i < n; i++){
+            int input_score;
+            scanf("%d", &input_score);
+            hashing[input_score]++;
+        }
+        int target_score;
+        scanf("%d", &target_score);
+        printf("%d\n", hashing[target_score]);
+        delete [] hashing;
+    }
+    return 0;
+}
+```
--- tmp.cpp
+++ tmp.cpp
@@ -1,35 +1,10 @@
 #include<iostream>
 using namespace std;
 
-class Demo{
-    int id;
-
-public:
-    Demo(int i){
-        id = i;
-        cout << "id = " << id << " constructed" << endl;
-    }
-    ~Demo(){
-        cout << "id = " << id << " destructed" << endl;
-    }
-};
-
-Demo d1(1);
-
-void fun(){
-    static Demo d2(2);
-    Demo d3(3);
-    cout << "fun" << endl;
-}
-
 int main(){
-    Demo d4(4);
-    d4 = 6;
-    cout << "main" << endl;
-    {
-        Demo d5(5);
+    int *parray = new int[100](1);
+    for(int i = 0; i < 100; i++){
+        printf("%d\n", parray[i]);
     }
-    fun();
-    cout << "main ends" << endl;
     return 0;
 }
