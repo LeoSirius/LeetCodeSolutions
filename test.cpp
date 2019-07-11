18e4a98d093fee3d1d9b3d7e59bf3eab7c1d1cf2
LeoSirius <is.li.xiaoyu@qq.com>
2019-07-11 11:35:57 +0800

    add

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "INT_MIN = " << INT_MIN << endl;
+    cout << "INT_MAX = " << INT_MAX << endl;
+    cout << "sizeof(int)" << sizeof(int) << endl;;
+    return 0;
+}
--- tmp.cpp
+++ tmp.cpp
@@ -1,56 +1,20 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-struct Node{
-    Node *left;
-    Node *right;
-    char v;
-};
-char str1[27], str2[27];    // 声明两个字符数组，分别保存输入的前序和中序遍历
-
-Node *create(){
-    // 申请一个结点空间，返回指向其的指针
-    Node *node = new Node();
-    node->left = node->right = nullptr;
-    return node;
-}
-
-void postorder(Node *T){
-    if(T == nullptr){
-        return;
-    }
-    postorder(T->left);
-    postorder(T->right);
-    printf("%c", T->v);
-}
-
-Node *build(int s1, int e1, int s2, int e2){
-    Node *node = create();
-    node->v = str1[s1];
-    int root_index;
-    for(int i = s2; i <= e2; ++i){
-        if(str1[s1] == str2[i]){
-            root_index = i;
-            break;
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend == INT_MIN && divisor == -1){
+            return INT_MAX;
         }
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while(dvd >= dvs){
+            long i_times_of_dvs = dvs, i = 1;
+            while(i_times_of_dvs << 1 <= dvd){
+                i_times_of_dvs <<= 1;
+                i <<= 1;
+            }
+            dvd -= i_times_of_dvs;
+            ans += i;
+        }
+        return sign * ans;
     }
-    // 下面分别先检查左右子树不为空，再进行构建
-    if(root_index != s2){
-        node->left = build(s1+1, s1+(root_index-s2), s2, root_index-1);
-    }
-    if(root_index != e2){
-        node->right = build(s1+(root_index-s2)+1, e1, root_index+1, e2);
-    }
-    return node;
-}
-
-int main(){
-    while(scanf("%s %s", str1, str2) == 2){
-        int size = strlen(str1);
-        Node *T = build(0, size-1, 0, size-1);
-        postorder(T);
-        printf("\n");
-    }
-    return 0;
-}
+};
