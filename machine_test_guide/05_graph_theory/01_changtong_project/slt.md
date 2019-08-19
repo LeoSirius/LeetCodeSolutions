da71dd8994b72df46413b2b23a1b9ae5a36addd8
LeoSirius <is.li.xiaoyu@qq.com>
2019-08-19 13:25:11 +0800

    update

--- machine_test_guide/05_graph_theory/01_changtong_project/slt.md
+++ machine_test_guide/05_graph_theory/01_changtong_project/slt.md
@@ -3,49 +3,43 @@
 首先确定一点，把n个结点连通，最少需要n-1个边。在这题中，我们先根据已经有的道路，计算出已有的连通分量的个数，这个值减1，就是要求的最少还需要的道路的数目。
 
 ```cpp
-#include<iostream>
+#include<algorithm>
 using namespace std;
 
-const int N = 10000001;
-int Tree[N];
-// sum[i]表示以结点i为根的树的结点个数，即结点i所在的连通分量的结点个数，
-// 其中保存的数据仅当Tree[i] = -1,即该结点为树的根结点时有效
-int sum[N];
-
-
+const int SIZE = 1001;
+int Tree[SIZE];
 int find_root (int x)
 {
     if (Tree[x] == -1) return x;
-    else {
+    else
+    {
         int root = find_root(Tree[x]);
         Tree[x] = root;
         return root;
     }
+    
 }
 
 int main ()
 {
-    int n;
-    while (scanf("%d", &n) != EOF) {
-        for (int i = 1; i < N; i++ ) {
-            Tree[i] = -1;
-            sum[i] = 1;
-        }
-        while (n--) {
+    int n, m;
+    while (scanf("%d %d", &n, &m) != EOF && n)
+    {
+        for (int i = 1; i <= n; i++) Tree[i] = -1;
+        while (m--)
+        {
             int a, b;
             scanf("%d %d", &a, &b);
             a = find_root(a);
             b = find_root(b);
-            if (a != b) {
-                Tree[a] = b;        // 将a树并到b树中
-                sum[b] += sum[a];   // 并将sum结点的计数加到b的sum中
-            }
+            if (a != b) Tree[a] = b;
         }
-        int res = 1;
-        for (int i = 1; i < N; i++) 
-            if (Tree[i] == -1 && res < sum[i])
-                res = sum[i];
-        printf("%d\n", res);
+
+        int num_of_connected = 0;
+        for (int i = 1; i <= n; i++)
+            if (Tree[i] == -1) num_of_connected++;
+
+        printf("%d\n", num_of_connected - 1);
     }
     return 0;
 }
--- tmp.cpp
+++ tmp.cpp
@@ -1,88 +1,40 @@
-#include<iostream>
-#include<vector>
+#include<algorithm>
 using namespace std;
 
-struct Node{
-    int idx;  // 邻接节点的编号
-    int dist;
-    int price;
-};
-
-vector<Node> path[1001];    // 注意是初始化vector的数组，而不是一个vector有1001元素
-
-int dists[1001];         // 距离数组
-int prices[1001];        // 花费数组
-bool is_solved[1001];   // 是否已经在解集中
+const int SIZE = 1001;
+int Tree[SIZE];
+int find_root (int x)
+{
+    if (Tree[x] == -1) return x;
+    else
+    {
+        int root = find_root(Tree[x]);
+        Tree[x] = root;
+        return root;
+    }
+    
+}
 
 int main ()
 {
-    int n, m, s, t;
-    while (scanf("%d %d", &n, &m) != EOF && (n || m))
+    int n, m;
+    while (scanf("%d %d", &n, &m) != EOF && n)
     {
-        for (int i = 1; i <= n; i++) path[i].clear();
+        for (int i = 1; i <= n; i++) Tree[i] = -1;
         while (m--)
         {
-            int a, b, d, p;
-            scanf("%d%d%d%d", &a, &b, &d, &p);
-            Node tmp_node;
-            tmp_node.dist = d;
-            tmp_node.price = p;
-            tmp_node.idx = b;
-            path[a].push_back(tmp_node);    // 无向图，把a到b和b到a都加入
-            tmp_node.idx = a;
-            path[b].push_back(tmp_node);
+            int a, b;
+            scanf("%d %d", &a, &b);
+            a = find_root(a);
+            b = find_root(b);
+            if (a != b) Tree[a] = b;
         }
-        scanf("%d%d", &s, &t);
 
-        // 初始化状态数组
+        int num_of_connected = 0;
         for (int i = 1; i <= n; i++)
-        {
-            dists[i] = -1;
-            prices[i] = 0;
-            is_solved[i] = false;
-        }
-        dists[s] = 0;
-        is_solved[s] = true;
-        int cur_node = s;
+            if (Tree[i] == -1) num_of_connected++;
 
-        // s已经加入，还有n-1趟
-        for (int i = 0; i < n - 1; i++)
-        {
-            // 遍历cur_node的所有邻接点，按情况更新状态数组
-            for (int j = 0; j < path[cur_node].size(); j++)
-            {
-                int adj_node = path[cur_node][j].idx;
-                if (is_solved[adj_node]) continue;
-                int dist = path[cur_node][j].dist;
-                int price = path[cur_node][j].price;
-
-                // 1.尚未计算过距离  2.之前的距离更长 3.距离相等，但是花费更小   
-                // 出现上面三种情况，则更新状态数组
-                if (dists[adj_node] == -1 || 
-                dists[adj_node] > dists[cur_node] + dist ||
-                (dists[adj_node] == dists[cur_node] + dist && prices[adj_node] > prices[cur_node] + price))
-                {
-                    dists[adj_node] = dists[cur_node] + dist;
-                    prices[adj_node] = prices[cur_node] + price;
-                }
-            }
-
-            // 当前这一趟更新完所有状态数组后，把距离最小的adj结点加入解集
-            // 花费在更新状态数组的时候已经考虑到了，所以这里直接按dists数组来选择即可
-            int min_dist = INT_MAX;
-            for (int j = 1; j <= n; j++)
-            {
-                if (is_solved[j]) continue;   // 已入解集，跳过
-                if (dists[j] == -1) continue; // 尚不连通，跳过
-                if (dists[j] < min_dist)
-                {
-                    min_dist = dists[j];
-                    cur_node = j;
-                }
-            }
-            is_solved[cur_node] = true;
-        }
-        printf("%d %d\n", dists[t], prices[t]);
+        printf("%d\n", num_of_connected - 1);
     }
     return 0;
 }
