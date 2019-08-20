b80cfd4bec8b722eb656afe22c2dee1c99a91c89
LeoSirius <is.li.xiaoyu@qq.com>
2019-08-20 10:25:49 +0800

    update

--- machine_test_guide/05_graph_theory/03_another_changtong_project/slt.md
+++ machine_test_guide/05_graph_theory/03_another_changtong_project/slt.md
@@ -10,17 +10,17 @@
 
 ```cpp
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-const int N = 101;
-int Tree[N];
+const int SIZE = 101;
+
+int Tree[SIZE];
 int find_root (int x)
 {
     if (Tree[x] == -1) return x;
-    else 
-    {
+    else {
         int root = find_root(Tree[x]);
         Tree[x] = root;
         return root;
@@ -29,41 +29,39 @@ int find_root (int x)
 
 struct Edge
 {
-    int a, b;
-    int weight;
-    bool operator < (const Edge& B) const
+    int a, b, weight;
+    bool operator < (const Edge& B) const  // 这里重载一定要是const函数，不然sort时报错
     {
         return weight < B.weight;
     }
 };
 
+
 int main ()
 {
     int n;
-    while (scanf("%d", &n) != EOF && n) 
-    {
+    while (scanf("%d", &n) != EOF && n) {
+
         vector<Edge> edges;         // notice vector start from 0, so use i-1, i base from 1
-        for (int i = 1; i <= n*(n-1)/2; i++) 
-        {
+        for (int i = 1; i <= n*(n-1)/2; i++) {
             Edge edge;
             scanf("%d%d%d", &edge.a, &edge.b, &edge.weight);
             edges.push_back(edge);
         }
         sort(edges.begin(), edges.end());
-        for (int i = 1; i <= N; i++) Tree[i] = -1;
 
-        int res = 0;
-        for (int i = 1; i <= n*(n-1)/2; i++) 
-        {
+        for (int i = 1; i <= SIZE; i++) Tree[i] = -1;
+
+        int min_total_weight = 0;
+        for (int i = 1; i <= n*(n-1)/2; i++)  {
             int a = find_root(edges[i-1].a);
             int b = find_root(edges[i-1].b);
-            if (a != b)
-            {
+            if (a != b) {
                 Tree[a] = b;
-                res += edges[i-1].weight;
+                min_total_weight += edges[i-1].weight;
             }
         }
-        printf("%d\n", res);
+        printf("%d\n", min_total_weight);
     }
     return 0;
 }
--- tmp.cpp
+++ tmp.cpp
@@ -1,36 +1,56 @@
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        if (s.size() <= 1) return s;
-        int max_len = 1, start = 0;
-        for (int i = 0; i < s.size();)
-        {
-            int left, right;
-            left = right = i;
-            while (right < s.size() - 1 && s[right] == s[right+1]) right++;
-            i = right + 1;
-            while (left > 0 && right < s.size() - 1 && s[left-1] == s[right+1])
-            {
-                left--; right++;
-            }
-            int new_len = right - left + 1;
-            if (new_len > max_len)
-            {
-                max_len = new_len;
-                start = max(start, left);
-            }
-            while(right < s.size()-1 && s[right+1] == s[right]) right++;
-            i = right + 1;    // 跳过重复的，下次i直接从这些重复的后面开始
-            while(right < s.size()-1 && left > 0 && s[right+1] == s[left-1]){
-                right++;
-                left--;
-            }
-            int new_len = right - left + 1;
-            if(new_len > max_len){
-                start = left;
-                max_len = new_len;
-            }
-         }
-        return s.substr(start, max_len);
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+const int SIZE = 101;
+
+int Tree[SIZE];
+int find_root (int x)
+{
+    if (Tree[x] == -1) return x;
+    else {
+        int root = find_root(Tree[x]);
+        Tree[x] = root;
+        return root;
+    }
+}
+
+struct Edge
+{
+    int a, b, weight;
+    bool operator < (const Edge& B) const  // 这里重载一定要是const函数，不然sort时报错
+    {
+        return weight < B.weight;
     }
 };
+
+
+int main ()
+{
+    int n;
+    while (scanf("%d", &n) != EOF && n) {
+
+        vector<Edge> edges;         // notice vector start from 0, so use i-1, i base from 1
+        for (int i = 1; i <= n*(n-1)/2; i++) {
+            Edge edge;
+            scanf("%d%d%d", &edge.a, &edge.b, &edge.weight);
+            edges.push_back(edge);
+        }
+        sort(edges.begin(), edges.end());
+
+        for (int i = 1; i <= SIZE; i++) Tree[i] = -1;
+
+        int min_total_weight = 0;
+        for (int i = 1; i <= n*(n-1)/2; i++)  {
+            int a = find_root(edges[i-1].a);
+            int b = find_root(edges[i-1].b);
+            if (a != b) {
+                Tree[a] = b;
+                min_total_weight += edges[i-1].weight;
+            }
+        }
+        printf("%d\n", min_total_weight);
+    }
+    return 0;
+}
