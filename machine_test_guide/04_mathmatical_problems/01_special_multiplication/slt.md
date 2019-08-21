333a862f4e6843576564dfab3e8d5750d37f2966
LeoSirius <is.li.xiaoyu@qq.com>
2019-08-21 10:41:23 +0800

    update

--- machine_test_guide/04_mathmatical_problems/01_special_multiplication/slt.md
+++ machine_test_guide/04_mathmatical_problems/01_special_multiplication/slt.md
@@ -1,19 +1,75 @@
 ### 思路1 直接双重循环
 
+结果可能会很大，我们用一个自定义的bigInt来存放。
+
 ```cpp
 #include<iostream>
+#include<string>
+#include<deque>
 using namespace std;
 
-int main(){
-    char a[11], b[11];
-    while(scanf("%s %s", a, b) != EOF){
-        int res;
-        for(int i = 0; a[i]; i++){
-            for(int j = 0; b[j]; j++){
-                res += (a[i] - '0') * (b[j] - '0');
+class bigInteger
+{
+private:
+    deque<int> value;
+public:
+    bigInteger () {};
+    bigInteger (int x) 
+    {
+        while (x) {
+            value.push_front(x % 10);
+            x /= 10;
+        }
+    }
+
+    void print () 
+    {
+        deque<int>::const_iterator iter;
+        for (iter = value.begin(); iter != value.end(); iter++) {
+            printf("%d", *iter);
+        }
+        printf("\n");
+    }
+
+    bigInteger operator + (bigInteger const &B) const
+    {
+        int carry = 0;
+        bigInteger res;
+        deque<int>::const_reverse_iterator iter, iter_B;
+        iter = value.rbegin();
+        iter_B = B.value.rbegin();
+
+        while (iter != value.rend() || iter_B != B.value.rend() || carry) {
+            int v1, v2;
+            v1 = v2 = 0;
+            if (iter != value.rend()) {
+                v1 = *iter;
+                iter++;
+            }
+            if (iter_B != B.value.rend()) {
+                v2 = *iter_B;
+                iter_B++;
+            }
+            int sum = v1 + v2 + carry;
+            res.value.push_front(sum % 10);
+            carry = sum / 10;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    string a, b;
+    while (cin >> a >> b) {
+        bigInteger res;
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                int cur_mutiple = (a[i] - '0') * (b[j] - '0');
+                res = res + cur_mutiple;
             }
         }
-        printf("%d\n", res);
+        res.print();
     }
     return 0;
 }
--- tmp.cpp
+++ tmp.cpp
@@ -1,91 +1,70 @@
 #include<iostream>
-#include<vector>
+#include<string>
+#include<deque>
 using namespace std;
 
-struct Node{
-    int adj;  // 邻接节点的编号
-    int dist;
-    int price;
+class bigInteger
+{
+private:
+    deque<int> value;
+public:
+    bigInteger () {};
+    bigInteger (int x) 
+    {
+        while (x) {
+            value.push_front(x % 10);
+            x /= 10;
+        }
+    }
+
+    void print () 
+    {
+        deque<int>::const_iterator iter;
+        for (iter = value.begin(); iter != value.end(); iter++) {
+            printf("%d", *iter);
+        }
+        printf("\n");
+    }
+
+    bigInteger operator + (bigInteger const &B) const
+    {
+        int carry = 0;
+        bigInteger res;
+        deque<int>::const_reverse_iterator iter, iter_B;
+        iter = value.rbegin();
+        iter_B = B.value.rbegin();
+
+        while (iter != value.rend() || iter_B != B.value.rend() || carry) {
+            int v1, v2;
+            v1 = v2 = 0;
+            if (iter != value.rend()) {
+                v1 = *iter;
+                iter++;
+            }
+            if (iter_B != B.value.rend()) {
+                v2 = *iter_B;
+                iter_B++;
+            }
+            int sum = v1 + v2 + carry;
+            res.value.push_front(sum % 10);
+            carry = sum / 10;
+        }
+        return res;
+    }
 };
 
-vector<Node> path[1001];    // 注意是初始化vector的数组，而不是一个vector有1001元素
-
-int dists[1001];         // 距离数组
-int prices[1001];        // 花费数组
-bool is_solved[1001];   // 是否已经在解集中
-
-int main ()
+int main()
 {
-    int n, m, s, t;
-    while (scanf("%d %d", &n, &m) != EOF && (n || m))
-    {
-        for (int i = 1; i <= n; i++) path[i].clear();
-        while (m--)
-        {
-            int a, b, d, p;
-            scanf("%d%d%d%d", &a, &b, &d, &p);
-            Node tmp_node;
-            tmp_node.dist = d;
-            tmp_node.price = p;
-            tmp_node.adj = b;
-            path[a].push_back(tmp_node);    // 无向图，把a到b和b到a都加入
-            tmp_node.adj = a;
-            path[b].push_back(tmp_node);
-        }
-        scanf("%d%d", &s, &t);
-
-        // 初始化状态数组
-        for (int i = 1; i <= n; i++)
-        {
-            dists[i] = -1;
-            prices[i] = 0;
-            is_solved[i] = false;
-        }
-        dists[s] = 0;
-        is_solved[s] = true;
-        int cur_node = s;
-
-        // s已经加入，还有n-1趟
-        for (int i = 0; i < n - 1; i++)
-        {
-            // 遍历cur_node的所有邻接点，按情况更新状态数组
-            for (int j = 0; j < path[cur_node].size(); j++)
-            {
-                int adj_node = path[cur_node][j].adj;
-                if (is_solved[adj_node]) continue;
-                int dist = path[cur_node][j].dist;
-                int price = path[cur_node][j].price;
-
-                int dist_via_cur = dists[cur_node] + dist;
-                int price_via_cur = prices[cur_node] + price;
-
-                // 1.尚未计算过距离  2.之前的距离更长 3.距离相等，但是花费更小   
-                // 出现上面三种情况，则更新状态数组
-                if (dists[adj_node] == -1 || 
-                dists[adj_node] > dist_via_cur ||
-                (dists[adj_node] == dist_via_cur && prices[adj_node] > price_via_cur))
-                {
-                    dists[adj_node] = dist_via_cur;
-                    prices[adj_node] = price_via_cur;
-                }
+    string a, b;
+    while (cin >> a >> b) {
+        bigInteger res;
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                int cur_mutiple = (a[i] - '0') * (b[j] - '0');
+                res = res + cur_mutiple;
             }
-
-            // 当前这一趟更新完所有状态数组后，把距离最小的adj结点加入解集
-            // 花费在更新状态数组的时候已经考虑到了，所以这里直接按dists数组来选择即可
-            int min_dist = INT_MAX;
-            for (int j = 1; j <= n; j++)
-            {
-                if (is_solved[j]) continue;   // 已入解集，跳过
-                if (dists[j] == -1) continue; // 尚不连通，跳过
-                if (dists[j] < min_dist)
-                {
-                    min_dist = dists[j];
-                    cur_node = j;
-                }
-            }
-            is_solved[cur_node] = true;
         }
-        printf("%d %d\n", dists[t], prices[t]);
+        res.print();
     }
     return 0;
 }
