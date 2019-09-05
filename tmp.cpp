b23c21d95a167e4907130d8b15ed0e1f9d7b505f
LeoSirius <is.li.xiaoyu@qq.com>
2019-09-05 17:56:45 +0800

    update

--- tmp.cpp
+++ tmp.cpp
@@ -1,20 +1,29 @@
-#include<iostream>
-using namespace std;
-
-int
-main ()
-{
-    int n;
-    while (scanf("%d", &n) != EOF && n != 0) {
-        int scores[101] = {0};
-        for (int i = 0; i < n; i++) {
-            int x;
-            scanf("%d", &x);
-            scores[x]++;
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        map<int, string> num2alpha = {
+            {2, "abc"},
+            {3, "def"},
+            {4, "ghi"},
+            {5, "jkl"},
+            {6, "mno"},
+            {7, "pqrs"},
+            {8, "tuv"},
+            {9, "wxyz"},
+        };
+        vector<string> res;
+        if (digits.size() == 0) return res;
+        res.push_back("");
+        for (int i = 0; i < digits.size(); i++) {
+            vector<string> tmp_res;
+            string chars = num2alpha[digits[i] - '0'];
+            for (int c = 0; c < chars.size(); c++) {
+                for (int j = 0; j < res.size(); j++) {
+                    tmp_res.push_back(res[j]+chars[c]);
+                }
+            }
+            res = tmp_res;
         }
-        int target;
-        scanf("%d", &target);
-        printf("%d\n", scores[target]);
+        return res;
     }
-    return 0;
-}
+};
