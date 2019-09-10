fc8a8b4272d6f13f40d733e905140f1f6f591583
LeoSirius <is.li.xiaoyu@qq.com>
2019-09-10 14:48:59 +0800

    update

--- tmp.cpp
+++ tmp.cpp
@@ -1,42 +1,17 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-using namespace std;
-
-struct Show
-{
-    int s, e;
-
-    bool operator < (Show const &other) const {
-        if (e != other.e) {
-            return e < other.e;
-        } else {
-            return s > other.s;
-        }
-    }
-};
-
-int
-main ()
-{
-    int n;
-    while (scanf("%d", &n) != EOF && n != 0) {
-        vector<Show> shows;
-        for (int i = 0; i < n; i++) {
-            Show show;
-            scanf("%d %d", &show.s, &show.e);
-            shows.push_back(show);
-        }
-        sort(shows.begin(), shows.end());
-        int cur_time = 0;
-        int show_cnt = 0;
-        for (int i = 0; i < shows.size(); i++) {
-            if (cur_time <= shows[i].s) {
-                show_cnt++;
-                cur_time = shows[i].e;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (int i = 0; s[i]; i++) {
+            switch(s[i]) {
+                case '(': stk.push(')'); break;
+                case '[': stk.push(']'); break;
+                case '{': stk.push('}'); break;
+                default:
+                    if (!stk.empty() && s[i] == stk.top()) stk.pop();
+                    else return false;
             }
         }
-        printf("%d\n", show_cnt);
+        return stk.empty();
     }
-    return EXIT_SUCCESS;
-}
+};
