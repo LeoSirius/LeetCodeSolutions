ba1d78eb0c06e1d6219cb542ce850c0c748cf574
LeoSirius <is.li.xiaoyu@qq.com>
2020-01-02 18:37:12 +0800

    update

--- tmp.cpp
+++ tmp.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int used_row[9][9] = {0};
-        int used_col[9][9] = {0};
-        int used_square[9][9] = {0};
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (board[i][j] != '.') {
-                    int k = i / 3 * 3 + j / 3;
-                    int num = board[i][j] - '0' - 1;
-                    if (used_row[i][num] || used_col[j][num] || used_square[k][num])
-                        return false;
-                    used_row[i][num] = used_col[j][num] = used_square[k][num] = 1;
-                }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr) return res;
+        stack<TreeNode*> stk;
+        TreeNode *cur_node = root;
+        while (cur_node != nullptr || !stk.empty()) {
+            if (cur_node) {
+                stk.push(cur_node);
+                cur_node = cur_node->left;
+            } else {
+                cur_node = stk.top();
+                stk.pop();
+                res.push_back(cur_node->val);
+                cur_node = cur_node->right;
             }
         }
-        return true;
+        return res;
     }
 };
 
-void test(string test_name, vector<vector<char>> &board, bool expected)
+void test(string test_name, TreeNode *tree, vector<int> expected)
 {
     Solution s;
-    if (s.isValidSudoku(board) == expected) {
+    if (s.inorderTraversal(tree) == expected) {
         cout << test_name << " success." << endl;
     } else {
         cout << test_name << " failed." << endl;
@@ -35,33 +44,11 @@ void test(string test_name, vector<vector<char>> &board, bool expected)
 
 int main()
 {
-    vector<vector<char>> board1 = {
-        {'5','3','.','.','7','.','.','.','.'},
-        {'6','.','.','1','9','5','.','.','.'},
-        {'.','9','8','.','.','.','.','6','.'},
-        {'8','.','.','.','6','.','.','.','3'},
-        {'4','.','.','8','.','3','.','.','1'},
-        {'7','.','.','.','2','.','.','.','6'},
-        {'.','6','.','.','.','.','2','8','.'},
-        {'.','.','.','4','1','9','.','.','5'},
-        {'.','.','.','.','8','.','.','7','9'},
-    };
-    bool expected1 = true;
-    test("test2", board1, expected1);
-
-    vector<vector<char>> board2 = {
-        {'8','3','.','.','7','.','.','.','.'},
-        {'6','.','.','1','9','5','.','.','.'},
-        {'.','9','8','.','.','.','.','6','.'},
-        {'8','.','.','.','6','.','.','.','3'},
-        {'4','.','.','8','.','3','.','.','1'},
-        {'7','.','.','.','2','.','.','.','6'},
-        {'.','6','.','.','.','.','2','8','.'},
-        {'.','.','.','4','1','9','.','.','5'},
-        {'.','.','.','.','8','.','.','7','9'}
-    };
-    bool expected2 = false;
-    test("test2", board2, expected2);
+    TreeNode *root1 = new TreeNode(1);
+    root1->right = new TreeNode(2);
+    root1->right->left = new TreeNode(3);
+    vector<int> expected1 = {1,3,2};
+    test("test1", root1, expected1);
 
     return 0;
 }
