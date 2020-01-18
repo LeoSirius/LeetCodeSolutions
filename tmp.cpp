7c9d581b2776c694e6ff918ca67d8b97b22e8209
LeoSirius <is.li.xiaoyu@qq.com>
2020-01-18 20:18:13 +0800

    update

--- tmp.cpp
+++ tmp.cpp
@@ -1,52 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
-class Solution {
+/*
+// Definition for a Node.
+class Node {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for (auto str : strs) {
-            string tmp = str;
-            sort(tmp.begin(), tmp.end());
-            mp[tmp].push_back(str);
-        }
-        vector<vector<string>> res;
-        for (auto pair : mp) {
-            res.push_back(pair.second);
-        }
-        return res;
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
 };
-
-void test(string test_name, vector<string> &strs, vector<vector<string>> &expected)
-{
-    vector<vector<string>> res = Solution().groupAnagrams(strs);
-    for (int i = 0; i < res.size(); i++) {
-        sort(res[i].begin(), res[i].end());
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
     }
-    sort(res.begin(), res.end());
-    for (int i = 0; i < expected.size(); i++) {
-        sort(expected[i].begin(), expected[i].end());
-    }
-    sort(expected.begin(), expected.end());
-    if (res == expected) {
-        cout << test_name << " success." << endl;
-    } else {
-        cout << test_name << " failed." << endl;
-    }
-}
-
-int main()
-{
-    vector<string> strs1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    vector<vector<string>> expected1 = {
-        {"ate","eat","tea"},
-        {"nat","tan"},
-        {"bat"},
-    };
-    test("test1", strs1, expected1);
-
-    return 0;
-}
+};
