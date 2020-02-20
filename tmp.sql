8394e73eb7c4225d2057f0312dfd3486323771ad
LeoSirius <is.li.xiaoyu@qq.com>
2020-02-20 12:05:16 +0800

    update

--- tmp.sql
+++ tmp.sql
@@ -317,8 +317,62 @@ INSERT INTO courses VALUES
 | H       | Math       |
 | I       | Math       |
 
+Table: Sales
 
-X city opened a new cinema, many people would like to go to this cinema. The cinema also gives out a poster indicating the movies’ ratings and descriptions.
-Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating.
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+sale_id is the primary key of this table.
+product_id is a foreign key to Product table.
+Note that the price is per unit.
+Table: Product
 
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
  
+
+Write an SQL query that reports the total quantity sold for every product id.
+
+The query result format is in the following example:
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++--------------+----------------+
+| product_id   | total_quantity |
++--------------+----------------+
+| 100          | 22             |
+| 200          | 15             |
++--------------+----------------+
+
+SELECT s.product_id, SUM(s.quantity) AS total_quantity
+FROM Sales s JOIN Product p
+ON s.product_id=p.product_id
+GROUP BY s.product_id;
