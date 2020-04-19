f045cba01d5fcbb398392558c02b44ec270b79dd
LeoSirius <is.li.xiaoyu@qq.com>
2020-04-19 17:29:20 +0800

    update

--- tmp.sql
+++ tmp.sql
@@ -1,37 +1,47 @@
-In facebook, there is a follow table with two columns: followee, follower.
+Mary is a teacher in a middle school and she has a table seat storing students'' names and their corresponding seat ids.
 
-Please write a sql query to get the amount of each follower’s follower if he/she has one.
-
-For example:
-
-+-------------+------------+
-| followee    | follower   |
-+-------------+------------+
-|     A       |     B      |
-|     B       |     C      |
-|     B       |     D      |
-|     D       |     E      |
-+-------------+------------+
-
-SELECT f1.follower, COUNT(DISTINCT f2.follower) AS num
-FROM follow f1 JOIN follow f2
-ON f1.follower=f2.followee
-GROUP BY f1.follower;
-
-should output:
-+-------------+------------+
-| follower    | num        |
-+-------------+------------+
-|     B       |  2         |
-|     D       |  1         |
-+-------------+------------+
-Explaination:
-Both B and D exist in the follower list, when as a followee, B's follower is C and D, and D's follower is E. A does not exist in follower list.
+The column id is continuous increment.
  
 
+Mary wants to change seats for the adjacent students.
  
 
-Note:
-Followee would not follow himself/herself in all cases.
-Please display the result in follower's alphabet order.
+Can you write a SQL query to output the result for Mary?
+ 
+
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Abbot   |
+|    2    | Doris   |
+|    3    | Emerson |
+|    4    | Green   |
+|    5    | Jeames  |
++---------+---------+
+
+
+SELECT
+    CASE
+        WHEN MOD(id,2)!=0 AND id!=cnt THEN id+1
+        WHEN MOD(id,2)!=0 AND id=cnt THEN id
+        ELSE id-1
+    END AS id
+, student
+FROM seat s JOIN (SELECT COUNT(*) AS cnt FROM seat) AS t
+ORDER BY id;
+
+For the sample input, the output is:
+
+
+ 
+
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Doris   |
+|    2    | Abbot   |
+|    3    | Green   |
+|    4    | Emerson |
+|    5    | Jeames  |
++---------+---------+
 
