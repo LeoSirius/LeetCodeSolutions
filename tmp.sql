2b9b1e77e81451dd49e97d9ac11fc1fc5fc930f9
LeoSirius <is.li.xiaoyu@qq.com>
2020-04-24 10:51:47 +0800

    update

--- tmp.sql
+++ tmp.sql
@@ -1,60 +1,109 @@
-Write a SQL query to find the highest grade with its
- corresponding course for each student. In case of a tie,
-  you should find the course with the smallest course_id.
-   The output must be sorted by increasing student_id.
+Write an SQL query to find the average for daily
+ percentage of posts that got removed after being
+  reported as spam, rounded to 2 decimal places.
 
 The query result format is in the following example:
 
-Enrollments table:
-+------------+-------------------+
-| student_id | course_id | grade |
-+------------+-----------+-------+
-| 2          | 2         | 95    |
-| 2          | 3         | 95    |
-| 1          | 1         | 90    |
-| 1          | 2         | 99    |
-| 3          | 1         | 80    |
-| 3          | 2         | 75    |
-| 3          | 3         | 82    |
-+------------+-----------+-------+
+Actions table:
++---------+---------+-------------+--------+--------+
+| user_id | post_id | action_date | action | extra  |
++---------+---------+-------------+--------+--------+
+| 1       | 1       | 2019-07-01  | view   | null   |
+| 1       | 1       | 2019-07-01  | like   | null   |
+| 1       | 1       | 2019-07-01  | share  | null   |
+| 2       | 2       | 2019-07-04  | view   | null   |
+| 2       | 2       | 2019-07-04  | report | spam   |
+| 3       | 4       | 2019-07-04  | view   | null   |
+| 3       | 4       | 2019-07-04  | report | spam   |
+| 4       | 3       | 2019-07-02  | view   | null   |
+| 4       | 3       | 2019-07-02  | report | spam   |
+| 5       | 2       | 2019-07-03  | view   | null   |
+| 5       | 2       | 2019-07-03  | report | racism |
+| 5       | 5       | 2019-07-03  | view   | null   |
+| 5       | 5       | 2019-07-03  | report | racism |
++---------+---------+-------------+--------+--------+
 
-选出每个学生的最大分数
+Removals table:
++---------+-------------+
+| post_id | remove_date |
++---------+-------------+
+| 2       | 2019-07-20  |
+| 3       | 2019-07-18  |
++---------+-------------+
 
-SELECT student_id, course_id, MAX(grade)
-FROM Enrollments
-GROUP BY student_id;
+找到所有是spam的report
 
-再JOIN两表
+SELECT *
+FROM Actions
+WHERE extra='spam';
 
-SELECT e.student_id, MIN(e.course_id) as course_id, e.grade
-FROM Enrollments e JOIN (
-    SELECT student_id, course_id, MAX(grade) AS max_grade
-    FROM Enrollments
-    GROUP BY student_id
-) AS t
-ON e.student_id=t.student_id AND e.grade=t.max_grade
-GROUP BY e.student_id
-ORDER BY e.student_id;
+LEFT JOIN 关联两表
 
+SELECT *
+FROM (
+    SELECT *
+    FROM Actions
+    WHERE extra='spam'
+) AS t LEFT JOIN Removals r
+ON t.post_id=r.post_id;
+
+找到按日期分组每组年内部的比例。注意两个表都要去重
+
+SELECT 
+    COUNT(DISTINCT r.post_id) / COUNT(DISTINCT t.post_id) AS daily_percent
+FROM (
+    SELECT *
+    FROM Actions
+    WHERE extra='spam'
+) AS t LEFT JOIN Removals r
+ON t.post_id=r.post_id
+GROUP BY action_date;
+
+
+SELECT ROUND(AVG(daily_percent)*100, 2) AS average_daily_percent
+FROM (
+    SELECT 
+        COUNT(DISTINCT r.post_id) / COUNT(DISTINCT t.post_id) AS daily_percent
+    FROM (
+        SELECT *
+        FROM Actions
+        WHERE extra='spam'
+    ) AS t LEFT JOIN Removals r
+    ON t.post_id=r.post_id
+    GROUP BY action_date
+) AS t;
 
 
 
 Result table:
-+------------+-------------------+
-| student_id | course_id | grade |
-+------------+-----------+-------+
-| 1          | 2         | 99    |
-| 2          | 2         | 95    |
-| 3          | 3         | 82    |
-+------------+-----------+-------+
++-----------------------+
+| average_daily_percent |
++-----------------------+
+| 75.00                 |
++-----------------------+
+The percentage for 2019-07-04 is 50% because only one post of two spam reported posts was removed.
+The percentage for 2019-07-02 is 100% because one post was reported as spam and it was removed.
+The other days had no spam reports so the average is (50 + 100) / 2 = 75%
+Note that the output is only one number and that we do not care about the remove dates.
 
 
-Create table If Not Exists Enrollments (student_id int, course_id int, grade int);
-Truncate table Enrollments;
-insert into Enrollments (student_id, course_id, grade) values ('2', '2', '95');
-insert into Enrollments (student_id, course_id, grade) values ('2', '3', '95');
-insert into Enrollments (student_id, course_id, grade) values ('1', '1', '90');
-insert into Enrollments (student_id, course_id, grade) values ('1', '2', '99');
-insert into Enrollments (student_id, course_id, grade) values ('3', '1', '80');
-insert into Enrollments (student_id, course_id, grade) values ('3', '2', '75');
-insert into Enrollments (student_id, course_id, grade) values ('3', '3', '82');
+
+Create table If Not Exists Actions (user_id int, post_id int, action_date date, action ENUM('view', 'like', 'reaction', 'comment', 'report', 'share'), extra varchar(10));
+create table if not exists Removals (post_id int, remove_date date);
+Truncate table Actions;
+insert into Actions (user_id, post_id, action_date, action, extra) values ('1', '1', '2019-07-01', 'view', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('1', '1', '2019-07-01', 'like', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('1', '1', '2019-07-01', 'share', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('2', '2', '2019-07-04', 'view', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('2', '2', '2019-07-04', 'report', 'spam');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('3', '4', '2019-07-04', 'view', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('3', '4', '2019-07-04', 'report', 'spam');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('4', '3', '2019-07-02', 'view', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('4', '3', '2019-07-02', 'report', 'spam');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('5', '2', '2019-07-03', 'view', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('5', '2', '2019-07-03', 'report', 'racism');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('5', '5', '2019-07-03', 'view', 'None');
+insert into Actions (user_id, post_id, action_date, action, extra) values ('5', '5', '2019-07-03', 'report', 'racism');
+Truncate table Removals;
+insert into Removals (post_id, remove_date) values ('2', '2019-07-20');
+insert into Removals (post_id, remove_date) values ('3', '2019-07-18');
