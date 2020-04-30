cc0df5cb40c317570b1106479cb3ad3ef0c539bb
LeoSirius <is.li.xiaoyu@qq.com>
2020-04-30 22:56:53 +0800

    update

--- 0087_Scramble_String/Solution1.cpp
+++ 0087_Scramble_String/Solution1.cpp
@@ -6,26 +6,27 @@ public:
     bool isScramble(string s1, string s2) {
         if (s1 == s2)
             return true;
-        
-        char char_cnt[26] = {0};
+        char ch_cnt[26] = {0};
         for (int i = 0; s1[i]; ++i) {
-            char_cnt[s1[i] - 'a']++;
-            char_cnt[s2[i] - 'a']--;
+            ch_cnt[s1[i] - 'a']++;
+            ch_cnt[s2[i] - 'a']--;
         }
-        for (int i = 0; i < 26; i++)
-            if (char_cnt[i] != 0)
+        for (int i = 0; i < 26; ++i)
+            if (ch_cnt[i] != 0)
                 return false;
-        
+
         int len = s1.size();
-        for (int i = 1; s1[i]; ++i) {
+        for (int i = 1; i < len; ++i) {
+            // substr是左闭右开的
             if (isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i)))
                 return true;
-            if (isScramble(s1.substr(0, i), s2.substr(len-i)) && isScramble(s1.substr(i), s2.substr(0, len-i)))
+            if (isScramble(s1.substr(0, i), s2.substr(len-i)) && isScramble(s1.substr(i), s2.substr(0,len-i)))
                 return true;
         }
         return false;
     }
 };
+
 
 void test(string test_name, string s1, string s2, bool expected)
 {
@@ -46,6 +47,10 @@ int main()
     string s12 = "abcde", s22 = "caebd";
     bool expected2 = false;
     test("test2", s12, s22, expected2);
+
+    string s13 = "abc", s23 = "bca";
+    bool expected3 = true;
+    test("test3", s13, s23, expected3);
 
     return 0;
 }
--- tmp.cpp
+++ tmp.cpp
@@ -3,35 +3,35 @@ using namespace std;
 
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN) {
-            if (divisor == 1)
-                return INT_MIN;
-            else if (divisor == -1)
-                return INT_MAX;
-            else {
-                return ((divisor & 1) == 1) ? divide(dividend+1, divisor) : divide(dividend>>1, divisor>>1);
-            }
+    bool isScramble(string s1, string s2) {
+        if (s1 == s2)
+            return true;
+        char ch_cnt[26] = {0};
+        for (int i = 0; s1[i]; ++i) {
+            ch_cnt[s1[i] - 'a']++;
+            ch_cnt[s2[i] - 'a']--;
         }
-        if (divisor == INT_MIN)
-            return 0;
-        
-        int dvd = abs(dividend), dvs = abs(divisor);
-        int res = 0, x = 0;
-        while (dvs <= dvd) {
-            for (x = 0; (dvd >> 1) - (dvs << x) >= 0; ++x);
-            res += 1 << x;
-            dvd -= dvs << x;
+        for (int i = 0; i < 26; ++i)
+            if (ch_cnt[i] != 0)
+                return false;
+
+        int len = s1.size();
+        for (int i = 1; i < len; ++i) {
+            // substr是左闭右开的
+            if (isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i)))
+                return true;
+            if (isScramble(s1.substr(0, i), s2.substr(len-i)) && isScramble(s1.substr(i), s2.substr(0,len-i)))
+                return true;
         }
-        return (dividend > 0 == divisor > 0) ? res : -res;
+        return false;
     }
 };
 
 
-void test(string test_name, int dividend, int divisor, int expected)
+void test(string test_name, string s1, string s2, bool expected)
 {
-    Solution s;
-    if (s.divide(dividend, divisor) == expected) {
+    bool res = Solution().isScramble(s1, s2);
+    if (res == expected) {
         cout << test_name << " success." << endl;
     } else {
         cout << test_name << " failed." << endl;
@@ -40,25 +40,17 @@ void test(string test_name, int dividend, int divisor, int expected)
 
 int main()
 {
-    int dividend1 = 10;
-    int divisor1 = 3;
-    int expected1 = 3;
-    test("test1", dividend1, divisor1, expected1);
+    string s11 = "great", s21 = "rgeat";
+    bool expected1 = true;
+    test("test1", s11, s21, expected1);
 
-    int dividend2 = 7;
-    int divisor2 = -3;
-    int expected2 = -2;
-    test("test2", dividend2, divisor2, expected2);
+    string s12 = "abcde", s22 = "caebd";
+    bool expected2 = false;
+    test("test2", s12, s22, expected2);
 
-    int dividend3 = -1;
-    int divisor3 = -1;
-    int expected3 = 1;
-    test("test3", dividend3, divisor3, expected3);
-
-    int dividend4 = 2147483647;
-    int divisor4 = 1;
-    int expected4 = 2147483647;
-    test("test4", dividend4, divisor4, expected4);
+    string s13 = "abc", s23 = "bca";
+    bool expected3 = true;
+    test("test3", s13, s23, expected3);
 
     return 0;
 }
