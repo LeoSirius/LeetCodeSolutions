d1cb0ca89f4ac7bdd580d7fb2bc94bda847eefc9
LeoSirius <is.li.xiaoyu@qq.com>
2020-05-08 15:17:04 +0800

    update

--- 0122_Best_Time_to_Buy_and_Sell_Stock_II/Solution1.cpp
+++ 0122_Best_Time_to_Buy_and_Sell_Stock_II/Solution1.cpp
@@ -1,18 +1,17 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int total = 0;
-        for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] > prices[i-1])
-                total += prices[i] - prices[i-1];
-        }
+        for (int i = 1; i < prices.size(); ++i)
+            total += max(0, prices[i]-prices[i-1]);
         return total;
     }
 };
+
 
 void test(string test_name, vector<int>& prices, int expected)
 {
@@ -40,20 +39,3 @@ int main()
 
     return 0;
 }
-
-// Input: [7,1,5,3,6,4]
-// Output: 7
-// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
-//              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
-// Example 2:
-
-// Input: [1,2,3,4,5]
-// Output: 4
-// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-//              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
-//              engaging multiple transactions at the same time. You must sell before buying again.
-// Example 3:
-
-// Input: [7,6,4,3,1]
-// Output: 0
-// Explanation: In this case, no transaction is done, i.e. max profit = 0.
--- tmp.cpp
+++ tmp.cpp
@@ -1,42 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1;
-        int mid;
-        while (l < r) {
-            mid = l + (r - l) / 2;
-            if (nums[mid] < nums[mid+1])
-                l = mid + 1;
-            else
-                r = mid;
-        }
-        return l;
+    int maxProfit(vector<int>& prices) {
+        int total = 0;
+        for (int i = 1; i < prices.size(); ++i)
+            total += max(0, prices[i]-prices[i-1]);
+        return total;
     }
 };
 
-void test(string test_name, vector<int>& nums, vector<int> expected)
+
+void test(string test_name, vector<int>& prices, int expected)
 {
-    int res = Solution().findPeakElement(nums);
-    if (find(expected.begin(), expected.end(), res) != expected.end())
+    Solution s;
+    if (s.maxProfit(prices) == expected) {
         cout << test_name << " success." << endl;
-    else
+    } else {
         cout << test_name << " failed." << endl;
+    }
 }
 
 int main()
 {
-    vector<int> nums1 = {1,2,3,1};
-    vector<int> expected1 = {2};
-    test("test1", nums1, expected1);
+    vector<int> prices1 = {7,1,5,3,6,4};
+    int expected1 = 7;
+    test("test1", prices1, expected1);
 
-    vector<int> nums2 = {1,2,1,3,5,6,4};
-    vector<int> expected2 = {1, 5};
-    test("test2", nums2, expected2);
+    vector<int> prices2 = {1,2,3,4,5};
+    int expected2 = 4;
+    test("test2", prices2, expected2);
+
+    vector<int> prices3 = {7,6,4,3,1};
+    int expected3 = 0;
+    test("test3", prices3, expected3);
 
     return 0;
 }
