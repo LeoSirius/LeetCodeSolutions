e9ea0f45740157fd0eabf24925f16e9fce13e473
LeoSirius <is.li.xiaoyu@qq.com>
2020-05-19 18:27:37 +0800

    update

--- tmp.py
+++ tmp.py
@@ -1,18 +1,6 @@
 
-grp_uuid_pairs = {
-    'uuid1': 1,
-    'uuid2': 2,
-    'uuid7': 7,
-    'uuid3': 3,
-    'uuid11': 11,
-    'uuid0': 0,
-    'uuid-1': -1,
-    'uuid22': 22,
-}
+def ff():
+    a = 1
 
-reversed_dict_by_grp_id = {k: v for k, v in reversed(sorted(grp_uuid_pairs.items(), key=lambda item: item[1]))}
-
-print('reversed_dict_by_grp_id  = {}'.format(reversed_dict_by_grp_id))
-
-for k, v in reversed_dict_by_grp_id.items():
-    print('k = {}, v = {}'.format(k, v))
+res = ff()
+print('res = {}'.format(res))
