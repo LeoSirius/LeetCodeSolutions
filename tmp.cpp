7257237c976b529b7e48d2cbea5923f01fbe556a
LeoSirius <is.li.xiaoyu@qq.com>
2020-05-20 21:29:22 +0800

    update

--- tmp.cpp
+++ tmp.cpp
@@ -1,45 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 class Solution {
 public:
     vector<double> twoSum(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 6; j++) {
-
+        int dp[15][70];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= 6; i ++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i ++) {
+            for (int j = i; j <= 6*i; j ++) {
+                for (int cur = 1; cur <= 6; cur ++) {
+                    if (j - cur <= 0) {
+                        break;
+                    }
+                    dp[i][j] += dp[i-1][j-cur];
+                }
             }
         }
+        int all = pow(6, n);
+        vector<double> ret;
+        for (int i = n; i <= 6 * n; i ++) {
+            ret.push_back(dp[n][i] * 1.0 / all);
+        }
+        return ret;
     }
-};
+}; 
 
-// class Solution {
-// public:
-//     vector<double> twoSum(int n) {
-//         int dp[15][70];
-//         memset(dp, 0, sizeof(dp));
-//         for (int i = 1; i <= 6; i ++) {
-//             dp[1][i] = 1;
-//         }
-//         for (int i = 2; i <= n; i ++) {
-//             for (int j = i; j <= 6*i; j ++) {
-//                 for (int cur = 1; cur <= 6; cur ++) {
-//                     if (j - cur <= 0) {
-//                         break;
-//                     }
-//                     dp[i][j] += dp[i-1][j-cur];
-//                 }
-//             }
-//         }
-//         int all = pow(6, n);
-//         vector<double> ret;
-//         for (int i = n; i <= 6 * n; i ++) {
-//             ret.push_back(dp[n][i] * 1.0 / all);
-//         }
-//         return ret;
-//     }
-// }; 
+void test(string test_name, int n, vector<double> expected)
+{
+    vector<double> res = Solution().twoSum(n);
+    bool passed = true;
+    if (res.size() != expected.size())
+        
+    if (res == expected)
+        cout << test_name << " success." << endl;
+    else
+        cout << test_name << " failed." << endl;
+}
 
+
+int main()
+{
+    int n1 = 1;
+    vector<double> expected1 = {0.16667,0.16667,0.16667,0.16667,0.16667,0.16667};
+    test("test1", n1, expected1);
+
+    int n2 = 2;
+    vector<double> expected2 = {0.02778,0.05556,0.08333,0.11111,0.13889,0.16667,0.13889,0.11111,0.08333,0.05556,0.02778};
+    // test("test2", n2, expected2);
+
+    return 0;
+}
 
 
 
