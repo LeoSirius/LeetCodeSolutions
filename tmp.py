4baf95fb88d8e72e1b919c882af811259494ad6b
LeoSirius <is.li.xiaoyu@qq.com>
2020-06-02 16:30:11 +0800

    update

--- tmp.py
+++ tmp.py
@@ -1,7 +1,24 @@
+import requests
 
-a = 1
-print(id(a))
-print(id(a+1))
-a = a + 1
+headers = {'Authorization': 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTEzMzkzMDIsImR0YWJsZV91dWlkIjoiNWNmMjY4Yzk1M2MzNGQyYzgyZTI4OWU2MjM4Yjg5ZTciLCJ1c2VybmFtZSI6ImY5MWE5YzgyMGIyODQyNDliM2JiMzM5NmRjNjViZDY0QGF1dGgubG9jYWwiLCJwZXJtaXNzaW9uIjoicncifQ.qcNe0zIckDdEjc9omN2k1IHF7_GDKCdKoHNxCmnu7NI'}
+url = 'http://127.0.0.1:5000/api/v1/dtables/5cf268c953c34d2c82e289e6238b89e7/append-csv/'
+data = {
+    'table_name': 'Book1',
+}
+# f = open('/Users/leo/Downloads/Book1.csv')
+# content = f.read()
+# print('content = {}'.format(content))
+files = {
+    'csv_file': '/Users/leo/Downloads/Book1.csv'
+}
 
-print(id(a))
+print('files = {}'.format(files))
+
+try:
+    res = requests.post(url, headers=headers, data=data, files=files)
+except Exception as e:
+    print("E = {}".format(e))
+
+print(res.status_code)
+print(res.text)
+# f.close()
