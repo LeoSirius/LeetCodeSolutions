4561adedccb41b6b387d10e863d3d8ccb3bf12df
LeoSirius <is.li.xiaoyu@qq.com>
2020-06-22 10:01:28 +0800

    update

--- 0060_Permutation_Sequence/Solution1.cpp
+++ 0060_Permutation_Sequence/Solution1.cpp
@@ -5,23 +5,22 @@ using namespace std;
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> fac(n + 1, 1);
-        for (int i = 1; i <= n; i++) {
-            fac[i] = i * fac[i-1];       // fac[i] store i!
-        }
+        vector<int> fac(n+1, 1);
+        for (int i = 1; i <= n; i++)
+            fac[i] = i * fac[i-1];        // fac[i] store i!
 
-        vector<char> num_char(n, '0');    // from 1 to n
-        for (int i = 0; i < n; i++) {
-            num_char[i] = i + 1 + '0';
-        }
+        vector<char> num_char(n, '0');
+        for (int i = 0; i < n; i++)
+            num_char[i] = i + 1 + '0';    // from 1 to n 的字符，存在0-n-1的索引中
+
         string res = "";
-        k--;    // start from 0
-        for (int i = n-1; i >= 0; i--) {
-            int index = k / fac[i];
-            k -= index * fac[i];
+        k--;            // // start from 0
+        for (int i = n - 1; 0 <= i; i--) {
+            int idx = k / fac[i];
+            k -= idx * fac[i];
 
-            res += num_char[index];
-            num_char.erase(num_char.begin() + index);
+            res += num_char[idx];
+            num_char.erase(num_char.begin() + idx);
         }
         return res;
     }
@@ -29,8 +28,8 @@ public:
 
 void test(string test_name, int n, int k, string expected)
 {
-    Solution s;
-    if (s.getPermutation(n, k) == expected) {
+    string res = Solution().getPermutation(n, k);
+    if (res == expected) {
         cout << test_name << " success." << endl;
     } else {
         cout << test_name << " failed." << endl;
@@ -56,3 +55,21 @@ int main()
 
     return 0;
 }
+
+// 给出集合 [1,2,3,…,n]，其所有元素共有 n! 种排列。
+
+// 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+
+// "123"
+// "132"
+// "213"
+// "231"
+// "312"
+// "321"
+// 给定 n 和 k，返回第 k 个排列。
+
+// 说明：
+
+// 给定 n 的范围是 [1, 9]。
+// 给定 k 的范围是[1,  n!]。
+
--- tmp.cpp
+++ tmp.cpp
@@ -1,74 +1,75 @@
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
-    vector<vector<int>> ans;
-
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        perm(nums, 0, nums.size() - 1);
-        return ans;
-    }
-    
-    void perm(vector<int> nums, int left, int right) {
-        if (left == right)
-            ans.push_back(nums);
-        else {
-            for (int i = left; i <= right; i++) {
-                if (i != left && nums[left] == nums[i]) continue;  // 去重
-                swap(nums[left], nums[i]);
-                perm(nums, left + 1, right);
-            }
+    string getPermutation(int n, int k) {
+        vector<int> fac(n+1, 1);
+        for (int i = 1; i <= n; i++)
+            fac[i] = i * fac[i-1];        // fac[i] store i!
+
+        vector<char> num_char(n, '0');
+        for (int i = 0; i < n; i++)
+            num_char[i] = i + 1 + '0';    // from 1 to n 的字符，存在0-n-1的索引中
+
+        string res = "";
+        k--;            // // start from 0
+        for (int i = n - 1; 0 <= i; i--) {
+            int idx = k / fac[i];
+            k -= idx * fac[i];
+
+            res += num_char[idx];
+            num_char.erase(num_char.begin() + idx);
         }
+        return res;
     }
 };
 
-void test(string test_name, vector<int> &nums, vector<vector<int>> expected)
+void test(string test_name, int n, int k, string expected)
 {
-    vector<vector<int>> res = Solution().permuteUnique(nums);
-    sort(res.begin(), res.end());
-    sort(expected.begin(), expected.end());
-    if (res == expected)
+    string res = Solution().getPermutation(n, k);
+    if (res == expected) {
         cout << test_name << " success." << endl;
-    else
+    } else {
         cout << test_name << " failed." << endl;
+    }
 }
 
 int main()
 {
-    vector<int> nums1 = {1,1,2};
-    vector<vector<int>> expected1 = {
-        {1,1,2},
-        {1,2,1},
-        {2,1,1},
-    };
-    test("test1", nums1, expected1);
+    int n1 = 3;
+    int k1 = 3;
+    string expected1 = "213";
+    test("test1", n1, k1, expected1);
 
-    vector<int> nums2 = {3,3,0,3};
-    vector<vector<int>> expected2 = {
-        {0,3,3,3},
-        {3,0,3,3},
-        {3,3,0,3},
-        {3,3,3,0}
-    };
-    test("test2", nums2, expected2);
+    int n2 = 4;
+    int k2 = 9;
+    string expected2 = "2314";
+    test("test2", n2, k2, expected2);
+
+    int n3 = 4;
+    int k3 = 15;
+    string expected3 = "3214";
+    test("test3", n3, k3, expected3);
 
     return 0;
 }
 
-// Given a collection of numbers that might contain duplicates, 
-// return all possible unique permutations.
+// 给出集合 [1,2,3,…,n]，其所有元素共有 n! 种排列。
 
-// Example:
+// 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
 
-// Input: [1,1,2]
-// Output:
-// [
-//   [1,1,2],
-//   [1,2,1],
-//   [2,1,1]
-// ]
+// "123"
+// "132"
+// "213"
+// "231"
+// "312"
+// "321"
+// 给定 n 和 k，返回第 k 个排列。
+
+// 说明：
+
+// 给定 n 的范围是 [1, 9]。
+// 给定 k 的范围是[1,  n!]。
 
