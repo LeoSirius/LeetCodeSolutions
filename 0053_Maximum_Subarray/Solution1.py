699bc4ae56a65c32e95d000a17dd216445d51a8f
LeoSirius <is.li.xiaoyu@qq.com>
2020-07-09 13:14:03 +0800

    update

--- 0053_Maximum_Subarray/Solution1.py
+++ 0053_Maximum_Subarray/Solution1.py
@@ -1,13 +1,15 @@
-from typing import List
+from typing import *
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = nums[0]
-        dp = [nums[0]]
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+
         for i in range(1, len(nums)):
-            dp.append(max(dp[i-1], 0) + nums[i])
-            res = max(res, dp[i])
-        return res
+            dp[i] = max(0, dp[i-1]) + nums[i]
+        
+        return max(dp)
+
 
 def test(test_name, nums, expected):
     res = Solution().maxSubArray(nums)
