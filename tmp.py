56e54f43403ed8cd0f75a459758b2f0e6272fca8
LeoSirius <is.li.xiaoyu@qq.com>
2020-07-09 18:00:50 +0800

    update

--- tmp.py
+++ tmp.py
@@ -1,29 +1,40 @@
 from typing import *
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        dp = [0 for _ in range(len(nums))]
-        dp[0] = nums[0]
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        pass
 
-        for i in range(1, len(nums)):
-            dp[i] = max(0, dp[i-1]) + nums[i]
-        
-        return max(dp)
-
-
-def test(test_name, nums, expected):
-    res = Solution().maxSubArray(nums)
-    if res == expected:
-        print(test_name + ' success.')
-    else:
-        print(test_name + ' failed.')
-
+def test(test_name, )
 
 if __name__ == "__main__":
-    nums1 = [-2,1,-3,4,-1,2,1,-5,4]
-    expected1 = 6
-    test('test1', nums1, expected1)
+    pass
 
-    nums2 = [1]
-    expected2 = 1
-    test('test2', nums2, expected2)
+
+# You are given two integer arrays nums1 and nums2 sorted in ascending order
+#  and an integer k.
+
+# Define a pair (u,v) which consists of one element from the first array and 
+# one element from the second array.
+
+# Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+
+# Example 1:
+
+# Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+# Output: [[1,2],[1,4],[1,6]] 
+# Explanation: The first 3 pairs are returned from the sequence: 
+#              [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+# Example 2:
+
+# Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+# Output: [1,1],[1,1]
+# Explanation: The first 2 pairs are returned from the sequence: 
+#              [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+
+# Example 3:
+
+# Input: nums1 = [1,2], nums2 = [3], k = 3
+# Output: [1,3],[2,3]
+# Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+
