3117d465bfedd14ef4a3b1ec8f3cba08c260bf42
LeoSirius <is.li.xiaoyu@qq.com>
2020-07-10 18:14:47 +0800

    update

--- tmp.py
+++ tmp.py
@@ -1,71 +1,29 @@
-from typing import *
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
 
-class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n = len(matrix)
+# Subclass QMainWindow to customise your application's main window
+class MainWindow(QMainWindow):
 
-        def is_mid_after_k(mid):
-            i, j = 0, n-1
-            cnt = 0
-            while i < n and 0 <= j:
-                if matrix[i][j] <= mid:
-                    i += 1
-                    cnt += j + 1
-                else:
-                    j -= 1
-            return k <= cnt
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
 
-        left, right = matrix[0][0], matrix[-1][-1]
-        while left < right:
-            mid = left + (right - left) // 2
-            if is_mid_after_k(mid):
-                right = mid
-            else:
-                left = mid + 1
-        return left
+        self.setWindowTitle("My Awesome App")
+
+        label = QLabel("This is a PyQt5 window!")
+
+        # The `Qt` namespace has a lot of attributes to customise
+        # widgets. See: http://doc.qt.io/qt-5/qt.html
+        label.setAlignment(Qt.AlignCenter)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(label)
 
 
-def test(test_name, matrix, k, expected):
-    res = Solution().kthSmallest(matrix, k)
-    if res == expected:
-        print(test_name + ' success.')
-    else:
-        print(test_name + ' failed.')
+app = QApplication(sys.argv)
 
-if __name__ == "__main__":
-    matrix1 = [
-        [ 1,  5,  9],
-        [10, 11, 13],
-        [12, 13, 15]
-    ]
-    k1 = 8
-    expected1 = 13
-    test('test1', matrix1, k1, expected1)
+window = MainWindow()
+window.show()
 
-    matrix2 = [
-        [1,2],
-        [1,3]
-    ]
-    k2 = 2
-    expected2 = 1
-    test('test2', matrix2, k2, expected2)
-
-# Given a n x n matrix where each of the rows and columns are sorted in
-#  ascending order, find the kth smallest element in the matrix.
-
-# Note that it is the kth smallest element in the sorted order, 
-# not the kth distinct element.
-
-# Example:
-
-# matrix = [
-#    [ 1,  5,  9],
-#    [10, 11, 13],
-#    [12, 13, 15]
-# ],
-# k = 8,
-
-# return 13.
-# Note:
-# You may assume k is always valid, 1 ≤ k ≤ n2.
-
+app.exec_()
