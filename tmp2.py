8a336d5018d135ad192bfbc100e42bef004eba50
LeoSirius <is.li.xiaoyu@qq.com>
2020-07-15 18:29:21 +0800

    update

--- tmp2.py
+++ tmp2.py
@@ -1,25 +1,4 @@
-import aiohttp
-import asyncio
-loop = asyncio.get_event_loop()
+import mimetypes
 
-async def fetch(i):
-    while True:
-        print(i)
-
-connector = aiohttp.TCPConnector(limit_per_host=199)
-async def main():
-    # async with aiohttp.ClientSession(connector=connector) as session:
-    #     for i in range(2):
-    #         html = await fetch(session)
-        # print(html)
-    tasks = []
-
-    # Fetch all responses within one Client session,
-    # keep connection alive for all requests.
-    async with aiohttp.ClientSession(connector=connector) as session:
-        tasks = [loop.create_task(fetch(i)) for i in range (2)]
-        await asyncio.gather(*tasks)
-loop.run_until_complete(mainA())
-
-import traceback
-traceback.print_exc()
+res = mimetypes.guess_type('123.png')
+print(res)
