8a8cc4e8555ab7731e3d8eabf6ba9cf03a6375bd
LeoSirius <is.li.xiaoyu@qq.com>
2020-07-24 16:18:11 +0800

    update

--- tmp.py
+++ tmp.py
@@ -1,58 +1,33 @@
 from typing import *
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        seen_ones, seen_twice = 0, 0
-        for n in nums:
-            seen_ones = seen_ones ^ n & ~seen_twice
-            seen_twice = seen_twice ^ n & ~seen_ones
-        return seen_ones
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        pass
 
-# class Solution:
-#     def singleNumber(self, nums: List[int]) -> int:
-#         ones, twos = 0, 0  # 分别表示余数三种状态的第二位和第一位
-#         for num in nums:
-#             ones = ones ^ num & ~twos
-#             twos = twos ^ num & ~ones
 
-#         # 计算完之后，twos全部都是0
-#         # ones就是最后的答案
-#         # 因为3个1再模3就没有了，剩下的单个的1都在ones里，就是最后的结果
-#         return ones
-
-def test(test_name, nums, expected):
-    res = Solution().singleNumber(nums)
-    if res == expected:
+def test(test_name, s, expected):
+    res = Solution().findRepeatedDnaSequencess(s)
+    if sorted(res) == sorted(expected):
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
 
 
 if __name__ == "__main__":
-    nums1 = [2,2,3,2]
-    expected1 = 3
-    test("test1", nums1, expected1)
-
-    nums2 = [0,1,0,1,0,1,99]
-    expected2 = 99
-    test("test2", nums2, expected2)
+    s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+    expected = ["AAAAACCCCC", "CCCCCAAAAA"]
 
 
-# Given a non-empty array of integers, every element appears 
-# three times except for one, which appears exactly once. 
-# Find that single one.
+# All DNA is composed of a series of nucleotides abbreviated as A, C, G,
+#  and T, for example: "ACGAATTCCG". When studying DNA, 
+# it is sometimes useful to identify repeated sequences within the DNA.
 
-# Note:
+# Write a function to find all the 10-letter-long sequences (substrings)
+#  that occur more than once in a DNA molecule.
 
-# Your algorithm should have a linear runtime complexity. 
-# Could you implement it without using extra memory?
+# Example:
 
-# Example 1:
+# Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
 
-# Input: [2,2,3,2]
-# Output: 3
+# Output: ["AAAAACCCCC", "CCCCCAAAAA"]
 
-# Example 2:
-
-# Input: [0,1,0,1,0,1,99]
-# Output: 99
