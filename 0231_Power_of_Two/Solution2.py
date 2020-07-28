50ee88fe523073f7130ed4a87da69a8c3188eea9
LeoSirius <is.li.xiaoyu@qq.com>
2020-07-28 10:29:51 +0800

    update

--- 0231_Power_of_Two/Solution2.py
+++ 0231_Power_of_Two/Solution2.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and n & (n-1) == 0
-
+        return n > 0 and ((n & (n-1)) == 0)
 
 def test(test_name, n, expected):
     res = Solution().isPowerOfTwo(n)
@@ -9,6 +8,7 @@ def test(test_name, n, expected):
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
+
 
 if __name__ == "__main__":
     n1, expected1 = 1, True
@@ -22,3 +22,23 @@ if __name__ == "__main__":
 
     n4, expected4 = 0, False
     test('test4', n4, expected4)
+
+# Given an integer, write a function to determine if it is a power of two.
+
+# Example 1:
+
+# Input: 1
+# Output: true 
+# Explanation: 20 = 1
+
+# Example 2:
+
+# Input: 16
+# Output: true
+# Explanation: 24 = 16
+
+# Example 3:
+
+# Input: 218
+# Output: false
+
