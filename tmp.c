08ac5ce04f62e80ec08b4ea77e200a359eb3508a
LeoSirius <is.li.xiaoyu@qq.com>
2020-08-03 17:24:28 +0800

    update

--- tmp.c
+++ tmp.c
@@ -1,66 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include "meta_c/utils.h"
-
-char* reverseLeftWords(char* s, int n){
-    int len = strlen(s);
-    char *left_chars = malloc(sizeof(char) * n + 1);
-    memcpy(left_chars, s, n);
-    left_chars[n] = '\0';
-
-    for (int i = n; s[i]; i++)
-        s[i-n] = s[i];
-
-    int new_right_start = len - n;
-    for (int i = 0; i < n; i++)
-        s[i+new_right_start] = left_chars[i];
-    return s;
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize){
+    
 }
 
-void test(char *test_name, char *s, int n, char *expected)
+void test(const char *test_name, int *nums, int numsSize, int k, int *expected, int expectedSize)
 {
-    char *res = reverseLeftWords(s, n);
-    if (is_equal_str(expected, res))
-        printf("%s success.\n", test_name);
-    else
-        printf("%s failed.\n", test_name);
+    int returnSize;
+    char *res = maxSlidingWindow(nums, numsSize, k, &returnSize);
+    if (returnSize != expectedSize || !is_equal_array(res, expected)) {
+        printf("%s failed.\n");
+    } else {
+        printf("%s success.\n");
+    }
 }
-
-
 
 int main()
 {
-    char s1[] = "abcdefg";
-    int k1 = 2;
-    char expected1[] = "cdefgab";
-    test("test1", s1, k1, expected1);
-
-    char s2[] = "lrloseumgh";
-    int k2 = 6;
-    char expected2[] = "umghlrlose";
-    test("test2", s2, k2, expected2);
-
+    int nums1[] = {1,3,-1,-3,5,3,6,7};
+    int k1 = 3;
+    int expected1[] = {3,3,5,5,6,7};
+    test("test1", nums1, 8, k1, expected1, 6);
 
     return 0;
 }
 
-// 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
-// 请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，
-// 该函数将返回左旋转两位得到的结果"cdefgab"。
 
-// 示例 1：
+// 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
 
-// 输入: s = "abcdefg", k = 2
-// 输出: "cdefgab"
+// 示例:
 
-// 示例 2：
+// 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+// 输出: [3,3,5,5,6,7] 
 
-// 输入: s = "lrloseumgh", k = 6
-// 输出: "umghlrlose"
+// 解释: 
+
+//   滑动窗口的位置                最大值
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
 //  
 
-// 限制：
+// 提示：
 
-// 1 <= k < s.length <= 10000
-
+// 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
