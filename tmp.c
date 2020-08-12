e96e7b3d398516e8c1acf9dd7e34add850d1cdf6
LeoSirius <is.li.xiaoyu@qq.com>
2020-08-12 11:47:27 +0800

    update

--- tmp.c
+++ tmp.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize){
+
+}
+
+
+
 int main()
 {
+    vector<int> nums1 = {1,3,-1,-3,5,3,6,7};
+    int k1 = 3;
+    vector<int> expected1 = {3,3,5,5,6,7};
+    test("test1", nums1, k1, expected1);
 
-    int a = 1, b = -3;
-    printf("%d\n", 1 / 3);
     return 0;
 }
+
+
+
+// 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+
+// 示例:
+
+// 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+// 输出: [3,3,5,5,6,7] 
+// 解释: 
+
+//   滑动窗口的位置                最大值
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+//  
+
+// 提示：
+
+// 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
