4d15bec0d323f474afced795ad081c30e198cff4
LeoSirius <is.li.xiaoyu@qq.com>
2020-08-14 18:20:57 +0800

    update

--- tmp.py
+++ tmp.py
@@ -3,6 +3,7 @@ from typing import *
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
+        nums.sort()
         size = len(nums)
 
         def dfs(start, path):
@@ -18,12 +19,17 @@ class Solution:
                 path[i], path[start] = path[start], path[i]
         
         dfs(0, nums)
+        print(res)
         return res
 
 
 def test(test_name, nums, expected):
     res = Solution().permuteUnique(nums)
-    if res.sort() == expected.sort():
+    res.sort()
+    expected.sort()
+    print(res)
+    print(expected)
+    if res == expected:
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
@@ -36,7 +42,7 @@ if __name__ == "__main__":
         [1,2,1],
         [2,1,1],
     ];
-    test("test1", nums1, expected1);
+    # test("test1", nums1, expected1);
 
     nums2 = [3,3,0,3];
     expected2 = [
@@ -45,7 +51,19 @@ if __name__ == "__main__":
         [3,3,0,3],
         [3,3,3,0]
     ];
-    test("test2", nums2, expected2);
+    # test("test2", nums2, expected2);
+
+    nums3 = [2,2,1,1]
+    expected3 = [
+        [1,1,2,2],
+        [1,2,1,2],
+        [1,2,2,1],
+        [2,1,1,2],
+        [2,1,2,1],
+        [2,2,1,1]
+    ]
+    # [[1,1,2,2],[1,2,1,2],[1,2,2,1],[2,1,1,2],[2,1,2,1],[2,2,1,1]]
+    test('test3', nums3, expected3)
 
 
 # Given a collection of numbers that might contain duplicates, 
