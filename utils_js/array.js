9a6c123dcb16f7fa968213cfead262dd417a4ae9
LeoSirius <is.li.xiaoyu@qq.com>
2020-08-26 14:21:21 +0800

    update

--- utils_js/array.js
+++ utils_js/array.js
@@ -1,29 +1,21 @@
-// Warn if overriding existing method
-if(Array.prototype.equals)
-    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
-// attach the .equals method to Array's prototype to call it on any array
-Array.prototype.equals = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
+let is_equal_array = function(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2))
+    return false;
 
-    // compare lengths - can save a lot of time
-    if (this.length != array.length)
-        return false;
+  if (arr1.length != arr2.length)
+    return false;
 
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].equals(array[i]))
-                return false;       
-        }           
-        else if (this[i] != array[i]) { 
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;   
-        }           
-    }       
-    return true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!is_equal_array(arr1[i], arr2[i]))
+        return false;
+    } else {
+      if (arr1[i] != arr2[i])
+        return false;
+    }
+  }
+  return true;
 }
-// Hide method from for-in loops
-Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
+
+export {is_equal_array};
