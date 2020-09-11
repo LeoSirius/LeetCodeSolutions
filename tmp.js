c9843a2775d6bd8cd1c889f8eb296f00e8dc53ca
LeoSirius <is.li.xiaoyu@qq.com>
2020-09-11 13:12:08 +0800

    update

--- tmp.js
+++ tmp.js
@@ -1,78 +1,42 @@
-import { ArrayUtils } from './utils_js/array.js';
-
 /**
- * @param {number[][]} edges
- * @return {number[]}
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
  */
-var findRedundantConnection = function(edges) {
-  let parents = Array.from(Array(edges.length + 1).keys());  // 结点 从1开始，所以多一个
-
-  let find = (x) => {
-    if (parents[x] === x) return x;
-    return find(parents[x]);
-  }
-
-  for (let edge of edges) {
-    let a = find(edge[0]), b = find(edge[1]);
-    if (a === b) return edge;
-    parents[a] = b;
-  }
+var findKthLargest = function(nums, k) {
+  
 };
 
-function test(testName, edges, expected) {
-  let res = findRedundantConnection(edges);
-  if (ArrayUtils.isEqualArray(res, expected))
+function test(testName, nums, k, expected) {
+  let res = findKthLargest(nums, k);
+  if (res == expected)
     console.log(testName + ' success.');
   else
     console.log(testName + ' failed.');
 }
 
-//    1
-//   / \
-//  2 - 3
-let edges1 = [[1,2], [1,3], [2,3]];
-let expected1 = [2,3];
-test('test1', edges1, expected1);
 
-//  5 - 1 - 2
-//      |   |
-//      4 - 3
-let edges2 = [[1,2], [2,3], [3,4], [1,4], [1,5]];
-let expected2 = [1,4];
-test('test2', edges2, expected2);
+let nums1 = [3,2,1,5,6,4]
+let k1 = 2
+let expected1 = 5
+test('test1', nums1, k1, expected1)
+
+let nums2 = [3,2,3,1,2,4,5,5,6]
+let k2 = 4
+let expected2 = 4
+test('test2', nums2, k2, expected2)
 
 
-// In this problem, a tree is an undirected graph that is connected and has no cycles.
-
-// The given input is a graph that started as a tree with N nodes
-//  (with distinct values 1, 2, ..., N), with one additional edge added. 
-//  The added edge has two different vertices chosen from 1 to N,
-//   and was not an edge that already existed.
-
-// The resulting graph is given as a 2D-array of edges. Each element of edges is a pair
-//  [u, v] with u < v, that represents an undirected edge connecting nodes u and v.
-
-// Return an edge that can be removed so that the resulting graph is a tree of N nodes.
-//  If there are multiple answers, return the answer that occurs last in the given
-//   2D-array. The answer edge [u, v] should be in the same format, with u < v.
+// Find the kth largest element in an unsorted array. 
+// Note that it is the kth largest element in the sorted order, 
+// not the kth distinct element.
 
 // Example 1:
-// Input: [[1,2], [1,3], [2,3]]
-// Output: [2,3]
-// Explanation: The given undirected graph will be like this:
-//   1
-//  / \
-// 2 - 3
+// Input: [3,2,1,5,6,4] and k = 2
+// Output: 5
 
 // Example 2:
-// Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
-// Output: [1,4]
-// Explanation: The given undirected graph will be like this:
-// 5 - 1 - 2
-//     |   |
-//     4 - 3
-
+// Input: [3,2,3,1,2,4,5,5,6] and k = 4
+// Output: 4
 // Note:
-// The size of the input 2D-array will be between 3 and 1000.
-// Every integer represented in the 2D-array will be between 1 and N, where N is 
-// the size of the input array.
+// You may assume k is always valid, 1 ≤ k ≤ array's length.
