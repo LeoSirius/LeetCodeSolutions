7040056bfd22413b5a5812c27b807d02e0b17793
LeoSirius <is.li.xiaoyu@qq.com>
2020-10-17 11:00:36 +0800

    update

--- mst32_II_从上到下打印二叉树_II/Solution1.go
+++ mst32_II_从上到下打印二叉树_II/Solution1.go
@@ -16,6 +16,7 @@ type TreeNode = tree.TreeNode
  *     Right *TreeNode
  * }
  */
+
 func levelOrder(root *TreeNode) [][]int {
 	var res [][]int
 	var que []*TreeNode
@@ -25,7 +26,6 @@ func levelOrder(root *TreeNode) [][]int {
 	}
 
 	que = append(que, root)
-	depth := 1
 	for len(que) > 0 {
 		curLevelSize := len(que)
 		var row []int
@@ -40,13 +40,6 @@ func levelOrder(root *TreeNode) [][]int {
 				que = append(que, p.Right)
 			}
 		}
-
-		if depth % 2 == 0 {
-			for i, j := 0, len(row)-1; i < j; i, j = i+1, j-1 {
-				row[i], row[j] = row[j], row[i]
-			}
-		}
-		depth++
 		res = append(res, row)
 	}
 	return res
@@ -74,28 +67,7 @@ func main() {
 	root1.Right.Right = &TreeNode{Val:7}
 	expected1 := [][]int{
 		{3},
-		{20, 9},
+		{9, 20},
 		{15, 7}}
 	test("test1", root1, expected1)
-	
-	//            0
-	//        2      4
-	//      1      3  -1
-	//    5  1      6   8
-	root2 := &TreeNode{Val:0}
-	root2.Left = &TreeNode{Val:2}
-	root2.Left.Left = &TreeNode{Val:1}
-	root2.Left.Left.Left = &TreeNode{Val:5}
-	root2.Left.Left.Right = &TreeNode{Val:1}
-	root2.Right = &TreeNode{Val:4}
-	root2.Right.Left = &TreeNode{Val:3}
-	root2.Right.Left.Right = &TreeNode{Val:6}
-	root2.Right.Right = &TreeNode{Val:-1}
-	root2.Right.Right.Right = &TreeNode{Val:8}
-	expected2 := [][]int{
-		{0},
-		{4,2},
-		{1,3,-1},
-		{8,6,1,5}}
-	test("test2", root2, expected2)
 }
