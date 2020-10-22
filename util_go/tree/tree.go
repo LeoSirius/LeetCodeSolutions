aa624e29eb1c414657f66511371fc01e1b94033c
LeoSirius <is.li.xiaoyu@qq.com>
2020-10-22 11:50:10 +0800

    update

--- util_go/tree/tree.go
+++ util_go/tree/tree.go
@@ -5,3 +5,16 @@ type TreeNode struct {
 	Left *TreeNode
 	Right *TreeNode
 }
+
+func IsEqualTree(t1, t2 *TreeNode) bool {
+	if t1 == nil && t2 == nil {
+		return true
+	}
+	if t1 == nil || t2 == nil {
+		return false
+	}
+	if t1.Val != t2.Val {
+		return false
+	}
+	return IsEqualTree(t1.Left, t2.Left) && IsEqualTree(t1.Right, t2.Right)
+}
