1f8e722baf3220139e80d3ef2c46a79da1c3464d
LeoSirius <is.li.xiaoyu@qq.com>
2020-10-30 11:57:04 +0800

    update

--- util_py/list.py
+++ util_py/list.py
@@ -4,6 +4,7 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 def is_equal_list(l1: ListNode, l2: ListNode):
     if not l1 and not l2:
         return True
@@ -13,12 +14,14 @@ def is_equal_list(l1: ListNode, l2: ListNode):
         return False
     return is_equal_list(l1.next, l2.next)
 
+
 def print_list_with_length(lst, length):
     p = lst
     while p and length:
         print(p.val, end=', ')
         p = p.next
         length -= 1
+
 
 def print_list(lst: ListNode, length=0):
     p = lst
@@ -29,3 +32,12 @@ def print_list(lst: ListNode, length=0):
     while p:
         print(p.val, end=', ')
         p = p.next
+
+
+def build_list(arr):
+    head = ListNode(0)
+    p = head
+    for n in arr:
+        p.next = ListNode(n)
+        p = p.next
+    return head.next
