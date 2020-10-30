9ee6bd561190d02a5bfb8678097ba9c30fed1672
LeoSirius <is.li.xiaoyu@qq.com>
2020-10-30 09:58:20 +0800

    update

--- tmp.go
+++ tmp.go
@@ -2,114 +2,51 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
-type MinStack struct {
-	s []int
-	smin []int
-}
-
-/** initialize your data structure here. */
-func Constructor() MinStack {
-	return MinStack{}
-}
-
-func (this *MinStack) Push(x int)  {
-	this.s = append(this.s, x)
-	if len(this.smin) == 0 || x <= this.smin[len(this.smin)-1] {
-		this.smin = append(this.smin, x)
+func firstUniqChar(s string) byte {
+	mp := map[rune]int{}
+	for _, ch := range s {
+		if _, ok := mp[ch]; ok {
+			mp[ch]++
+		} else {
+			mp[ch] = 1
+		}
 	}
+	return byte('a')
 }
 
-
-func (this *MinStack) Pop() {
-	top := this.s[len(this.s)-1]
-	if top == this.smin[len(this.smin)-1] {
-		this.smin = this.smin[:len(this.smin)-1]
-	}
-	this.s = this.s[:len(this.s)-1]
-}
-
-func (this *MinStack) Top() int {
-	return this.s[len(this.s)-1]
-
-}
-
-func (this *MinStack) Min() int {
-	return this.smin[len(this.smin)-1]
-}
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(x);
- * obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.Min();
- */
-
-func test1() {
-	stk := Constructor()
-	stk.Push(-2)
-	stk.Push(0)
-	stk.Push(-3)
-	res1 := stk.Min()   // -3
-	stk.Pop()
-	res2 := stk.Top()   // 0
-	res3 := stk.Min()   // -2
-	
-	a := []int{res1, res2, res3}
-	b := []int{-3, 0, -2}
-	if reflect.DeepEqual(a, b) {
-		fmt.Println("test1 success.")
+func test(testName string, s string, expected byte) {
+	res := firstUniqChar(s)
+	if res == expected {
+		fmt.Println(testName + " success.")
 	} else {
-		fmt.Println("test1 failed.")
-	}
-}
-
-func test2() {
-	stk := Constructor()
-	stk.Push(0)
-	stk.Push(1)
-	stk.Push(0)
-	res1 := stk.Min()      // 0
-	stk.Pop()
-	res2 := stk.Min()      // 0
-
-	a := []int{res1, res2}
-	b := []int{0, 0}
-	if reflect.DeepEqual(a, b) {
-		fmt.Println("test2 success.")
-	} else {
-		fmt.Println("test2 failed.")
+		fmt.Println(testName + " failed.")
 	}
 }
 
 func main() {
-	test1()
-	test2()
+	s1 := "abaccdeff"
+	expected1 := byte('b')
+	test("test1", s1, expected1)
+
+
+	s2 := ""
+	expected2 := byte(' ')
+	test("test2", s2, expected2)
 }
 
 
-// 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，
-// 调用 min、push 及 pop 的时间复杂度都是 O(1)。
+// 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
 
 // 示例:
-// MinStack minStack = new MinStack();
-// minStack.push(-2);
-// minStack.push(0);
-// minStack.push(-3);
-// minStack.min();   --> 返回 -3.
-// minStack.pop();
-// minStack.top();      --> 返回 0.
-// minStack.min();   --> 返回 -2.
+// s = "abaccdeff"
+// 返回 "b"
+
+// s = "" 
+// 返回 " "
 //  
 
-// 提示：
-
-// 各函数的调用总次数不超过 20000 次
-//  
-
-// 注意：本题与主站 155 题相同
+// 限制：
+// 0 <= s 的长度 <= 50000
 
