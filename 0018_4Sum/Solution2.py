403b16e487ae87ec0a5992714f2b866392d585c6
LeoSirius <is.li.xiaoyu@qq.com>
2021-03-12 19:51:46 +0800

    update

--- 0018_4Sum/Solution2.py
+++ 0018_4Sum/Solution2.py
@@ -1,18 +1,18 @@
-from typing import *
+from typing import List
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        def n_sum(nums, target, N, path, res):
-            if len(nums) < N or N < 2 or N * nums[-1] < target or target < N * nums[0]:
+
+        def N_sum(N, nums, target, path, res):
+            if N < 2 or len(nums) < N or N * nums[0] > target or N * nums[-1] < target:
                 return
 
-            if 2 < N:
+            if N > 2:
                 for i in range(len(nums) - N + 1):
                     if i > 0 and nums[i] == nums[i-1]: continue
-                    n_sum(nums[i+1:], target - nums[i], N - 1, path + [nums[i]], res)
+                    N_sum(N - 1, nums[i+1:], target - nums[i], path + [nums[i]], res)
                 return
 
-            # N == 2
             l, r = 0, len(nums) - 1
             while l < r:
                 s = nums[l] + nums[r]
@@ -26,19 +26,19 @@ class Solution:
                     l += 1
                 else:
                     r -= 1
-        
+
         res = []
         nums.sort()
-        n_sum(nums, target, 4, [], res)
+        N_sum(4, nums, target, [], res)
         return res
+
 
 def test(test_name, nums, target, expected):
     res = Solution().fourSum(nums, target)
     for item in res:
         item.sort()
     res.sort()
-    for item in expected:
-        item.sort()
+    expected = [sorted(item) for item in expected]
     expected.sort()
     if res == expected:
         print(test_name + ' success.')
@@ -55,3 +55,11 @@ if __name__ == '__main__':
         [-2,  0, 0, 2]
     ]
     test('test1', nums1, target1, expected1)
+
+    nums2 = [-2,-1,-1,1,1,2,2]
+    target2 = 0
+    expected2 = [[
+        -2,-1,1,2],
+        [-1,-1,1,1]]
+    test('test2', nums2, target2, expected2)
+
--- t.py
+++ t.py
@@ -1,19 +1,45 @@
+from typing import List
+
 class Solution:
-    def isValidSerialization(self, preorder: str) -> bool:
-        diff = 1   # diff = 出度 - 入度。根节点没有入度，根节点1-1+2=2，正好是一开始的度数。
-        nodes = preorder.split(',')
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 
-        for node in nodes:
-            diff -= 1
-            if diff < 0:
-                return False
-            if node != '#':
-                diff += 2
-        return diff == 0
+        def N_sum(N, nums, target, path, res):
+            if N < 2 or len(nums) < N or N * nums[0] > target or N * nums[-1] < target:
+                return
+
+            if N > 2:
+                for i in range(len(nums) - N + 1):
+                    if i > 0 and nums[i] == nums[i-1]: continue
+                    N_sum(N - 1, nums[i+1:], target - nums[i], path + [nums[i]], res)
+                return
+
+            l, r = 0, len(nums) - 1
+            while l < r:
+                s = nums[l] + nums[r]
+                if s == target:
+                    res.append(path + [nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]: l += 1
+                    while l < r and nums[r] == nums[r+1]: r -= 1
+                elif s < target:
+                    l += 1
+                else:
+                    r -= 1
+
+        res = []
+        nums.sort()
+        N_sum(4, nums, target, [], res)
+        return res
 
 
-def test(test_name, preorder, expected):
-    res = Solution().isValidSerialization(preorder)
+def test(test_name, nums, target, expected):
+    res = Solution().fourSum(nums, target)
+    for item in res:
+        item.sort()
+    res.sort()
+    expected = [sorted(item) for item in expected]
+    expected.sort()
     if res == expected:
         print(test_name + ' success.')
     else:
@@ -21,19 +47,18 @@ def test(test_name, preorder, expected):
 
 
 if __name__ == '__main__':
-    preorder1 = '9,3,4,#,#,1,#,#,2,#,6,#,#'
-    expected1 = True
-    test('test1', preorder1, expected1)
+    nums1 = [1, 0, -1, 0, -2, 2]
+    target1 = 0
+    expected1 = [
+        [-1,  0, 0, 1],
+        [-2, -1, 1, 2],
+        [-2,  0, 0, 2]
+    ]
+    test('test1', nums1, target1, expected1)
 
-    preorder2 = '1,#'
-    expected2 = False
-    test('test2', preorder2, expected2)
-
-    preorder3 = '9,#,#,1'
-    expected3 = False
-    test('test3', preorder3, expected3)
-
-    preorder4 = '#,#,3,5,#'
-    expected4 = False
-    test('test4', preorder4, expected4)
-
+    nums2 = [-2,-1,-1,1,1,2,2]
+    target2 = 0
+    expected2 = [[
+        -2,-1,1,2],
+        [-1,-1,1,1]]
+    test('test2', nums2, target2, expected2)
