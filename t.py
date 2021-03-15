cdee4e6b4802ee6f6aa74f4de7b98a6eab71801e
LeoSirius <is.li.xiaoyu@qq.com>
2021-03-15 22:32:26 +0800

    update

--- t.py
+++ t.py
@@ -1,64 +1,84 @@
-from typing import List
+from typing import *
+
 
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-
-        def N_sum(N, nums, target, path, res):
-            if N < 2 or len(nums) < N or N * nums[0] > target or N * nums[-1] < target:
-                return
-
-            if N > 2:
-                for i in range(len(nums) - N + 1):
-                    if i > 0 and nums[i] == nums[i-1]: continue
-                    N_sum(N - 1, nums[i+1:], target - nums[i], path + [nums[i]], res)
-                return
-
-            l, r = 0, len(nums) - 1
-            while l < r:
-                s = nums[l] + nums[r]
-                if s == target:
-                    res.append(path + [nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l-1]: l += 1
-                    while l < r and nums[r] == nums[r+1]: r -= 1
-                elif s < target:
-                    l += 1
-                else:
-                    r -= 1
-
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
-        nums.sort()
-        N_sum(4, nums, target, [], res)
+        if not matrix:
+            return res
+
+        left, right = 0, len(matrix[0]) - 1
+        top, bottom = 0, len(matrix) - 1
+
+        while left <= right and top <= bottom:
+            for i in range(left, right+1):
+                res.append(matrix[top][i])
+            top += 1
+
+            for j in range(top, bottom+1):
+                res.append(matrix[j][right])
+            right -= 1
+
+            if left > right or top > bottom:
+                break
+
+            for i in range(right, left-1, -1):
+                res.append(matrix[bottom][i])
+            bottom -= 1
+
+            for j in range(bottom, top-1, -1):
+                res.append(matrix[j][left])
+            left += 1
+
         return res
 
 
-def test(test_name, nums, target, expected):
-    res = Solution().fourSum(nums, target)
-    for item in res:
-        item.sort()
-    res.sort()
-    expected = [sorted(item) for item in expected]
-    expected.sort()
+def test(test_name, matrix, expected):
+    res = Solution().spiralOrder(matrix)
+    print(f'res = {res}')
     if res == expected:
-        print(test_name + ' success.')
+        print(f'{test_name} success.')
     else:
-        print(test_name + ' failed.')
+        print(f'{test_name} failed.')
 
 
-if __name__ == '__main__':
-    nums1 = [1, 0, -1, 0, -2, 2]
-    target1 = 0
-    expected1 = [
-        [-1,  0, 0, 1],
-        [-2, -1, 1, 2],
-        [-2,  0, 0, 2]
+if __name__ == "__main__":
+    matrix1 = [
+        [ 1, 2, 3 ],
+        [ 4, 5, 6 ],
+        [ 7, 8, 9 ]
     ]
-    test('test1', nums1, target1, expected1)
+    expected1 = [1,2,3,6,9,8,7,4,5]
+    test('test1', matrix1, expected1)
 
-    nums2 = [-2,-1,-1,1,1,2,2]
-    target2 = 0
-    expected2 = [[
-        -2,-1,1,2],
-        [-1,-1,1,1]]
-    test('test2', nums2, target2, expected2)
+    matrix2 = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9,10,11,12]
+    ]
+    expected2 = [1,2,3,4,8,12,11,10,9,5,6,7]
+    test('test2', matrix2, expected2)
+
+
+# Given a matrix of m x n elements (m rows, n columns), return
+#  all elements of the matrix in spiral order.
+
+# Example 1:
+
+# Input:
+# [
+#  [ 1, 2, 3 ],
+#  [ 4, 5, 6 ],
+#  [ 7, 8, 9 ]
+# ]
+# Output: [1,2,3,6,9,8,7,4,5]
+# Example 2:
+
+# Input:
+# [
+#   [1, 2, 3, 4],
+#   [5, 6, 7, 8],
+#   [9,10,11,12]
+# ]
+# Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
