ab68f67befaf7a3be64e50619eb1ad72daf987e2
LeoSirius <is.li.xiaoyu@qq.com>
2021-03-20 20:48:03 +0800

    update

--- 0150_Evaluate_Reverse_Polish_Notation/Solution1.py
+++ 0150_Evaluate_Reverse_Polish_Notation/Solution1.py
@@ -1,30 +1,27 @@
 from typing import *
 import math
 
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stk = []
-
-        for char in tokens:
-            try:
-                stk.append(int(char))
-            except ValueError:
+        for token in tokens:
+            if token in ['+', '-', '*', '/']:
                 b, a = stk.pop(), stk.pop()
-                if char == '+':
-                    stk.append(a + b)
-                elif char == '-':
-                    stk.append(a - b)
-                elif char == '*':
-                    stk.append(a * b)
+                if token == '+':
+                    stk.append(a+b)
+                elif token == '-':
+                    stk.append(a-b)
+                elif token == '*':
+                    stk.append(a*b)
                 else:
-                    stk.append(math.trunc(a / b))
-                # >>> 1 // -3             在c中  1 / -3的结果就是0
-                # -1
-                # >>> 1 / -3
-                # -0.3333333333333333
-                # >>> math.trunc(1 / -3)  注意Python中负数也是向下取整，这里需要用trunc来趋向0
-                # 0
-        return stk[-1]
+                    # 這裡是 python 複數整除的一個坑，10 // -3 = -4
+                    # 複數除不盡的應該是向上取整，但是 python 中複數也是向下
+                    stk.append(math.trunc(a/b))
+            else:
+                stk.append(int(token))
+
+        return int(stk[0])
 
 
 def test(test_name, tokens, expected):
@@ -38,15 +35,23 @@ def test(test_name, tokens, expected):
 if __name__ == "__main__":
     tokens1 = ["2", "1", "+", "3", "*"]
     expected1 = 9;
-    test("test1", tokens1, expected1);
+    test('test1', tokens1, expected1)
 
     tokens2 = ["4", "13", "5", "/", "+"]
     expected2 = 6;
-    test("test2", tokens2, expected2);
+    test('test2', tokens2, expected2)
 
     tokens3 = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
     expected3 = 22;
-    test("test3", tokens3, expected3);
+    test('test3', tokens3, expected3)
+
+    tokens4 = ["2","1","+","3","*"]
+    expected4 = 9
+    test('test4', tokens4, expected4)
+
+    tokens5 = ["4","-2","/","2","-3","-","-"]
+    expected5 = -7
+    test('test5', tokens5, expected5)
 
 
 # Evaluate the value of an arithmetic expression in Reverse Polish Notation.
--- t.py
+++ t.py
@@ -1,62 +1,89 @@
 from typing import *
+import math
 
 
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk = []
+        for token in tokens:
+            if token in ['+', '-', '*', '/']:
+                b, a = stk.pop(), stk.pop()
+                if token == '+':
+                    stk.append(a+b)
+                elif token == '-':
+                    stk.append(a-b)
+                elif token == '*':
+                    stk.append(a*b)
+                else:
+                    # 這裡是 python 複數整除的一個坑，10 // -3 = -4
+                    # 複數除不盡的應該是向上取整，但是 python 中複數也是向下
+                    stk.append(math.trunc(a/b))
+            else:
+                stk.append(int(token))
 
-        # idx_max: 最大元素的idx
-        # idx_big: 從右向左 比 idx_max - 1 元素大的 idx
-
-        # 兩種特殊情況
-        # case1：降序，312，在第一個循環裡i == 0時返回
-        # case2：正序，123，這時 idx_max 和 idx_big 都等於最後一個元素。swap 最後兩個元素，然後結束
-        for i in range(len(nums) - 1, -1, -1):
-            if i == 0:
-                nums.reverse()
-                return
-            if nums[i-1] < nums[i]:
-                idx_max = i
-                break
-
-        idx_big = len(nums) - 1
-        for i in range(len(nums) - 1, idx_max - 1, -1):
-            if nums[i] > nums[idx_max-1]:
-                idx_big = i
-                break
-
-        nums[idx_big], nums[idx_max-1] = nums[idx_max-1], nums[idx_big]
-
-        i, j = idx_max, len(nums) - 1
-        while i < j:
-            nums[i], nums[j] = nums[j], nums[i]
-            i += 1
-            j -= 1
+        return int(stk[0])
 
 
-def test(test_name, nums, expected):
-    Solution().nextPermutation(nums)
-    if nums == expected:
+def test(test_name, tokens, expected):
+    res = Solution().evalRPN(tokens)
+    if res == expected:
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
 
 
-if __name__ == '__main__':
-    nums1 = [1,2,3]
-    expected1 = [1,3,2]
-    test("test1", nums1, expected1)
+if __name__ == "__main__":
+    tokens1 = ["2", "1", "+", "3", "*"]
+    expected1 = 9;
+    test('test1', tokens1, expected1)
 
-    nums2 = [3,2,1]
-    expected2 = [1,2,3]
-    test("test2", nums2, expected2)
+    tokens2 = ["4", "13", "5", "/", "+"]
+    expected2 = 6;
+    test('test2', tokens2, expected2)
 
-    nums3 = [1,1,5]
-    expected3 = [1,5,1]
-    test("test3", nums3, expected3)
+    tokens3 = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+    expected3 = 22;
+    test('test3', tokens3, expected3)
 
-    nums4 = [1,3,2]
-    expected4 = [2,1,3]
-    test("test4", nums4, expected4)
+    tokens4 = ["2","1","+","3","*"]
+    expected4 = 9
+    test('test4', tokens4, expected4)
+
+    tokens5 = ["4","-2","/","2","-3","-","-"]
+    expected5 = -7
+    test('test5', tokens5, expected5)
+
+
+# Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+# Valid operators are +, -, *, /. 
+# Each operand may be an integer or another expression.
+
+# Note:
+
+# Division between two integers should truncate toward zero.
+# The given RPN expression is always valid. 
+# That means the expression would always evaluate to a result and
+# there won't' be any divide by zero operation.
+# Example 1:
+
+# Input: ["2", "1", "+", "3", "*"]
+# Output: 9
+# Explanation: ((2 + 1) * 3) = 9
+# Example 2:
+
+# Input: ["4", "13", "5", "/", "+"]
+# Output: 6
+# Explanation: (4 + (13 / 5)) = 6
+# Example 3:
+
+# Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+# Output: 22
+# Explanation: 
+#   ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+# = ((10 * (6 / (12 * -11))) + 17) + 5
+# = ((10 * (6 / -132)) + 17) + 5
+# = ((10 * 0) + 17) + 5
+# = (0 + 17) + 5
+# = 17 + 5
+# = 22
