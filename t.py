2f9d2d6d56898a38a6281afb305f9228251ad5d4
LeoSirius <is.li.xiaoyu@qq.com>
2021-04-06 12:38:17 +0800

    update

--- t.py
+++ t.py
@@ -1,36 +1,29 @@
 from typing import *
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
+        res = nums[0] + nums[1] + nums[2]
 
-        for i, n in enumerate(nums):
+        for i in range(len(nums) - 2):
             l, r = i + 1, len(nums) - 1
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
             while l < r:
-                _sum = nums[l] + nums[r] + n
-                if _sum == 0:
-                    res.append([nums[l], nums[r], n])
+                _sum = nums[i] + nums[l] + nums[r]
+                if _sum > target:
+                    r -= 1
+                elif _sum < target:
                     l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l-1]: l += 1
-                    while l < r and nums[r] == nums[r+1]: r -= 1
-                elif _sum > 0:
-                    r -= 1
                 else:
-                    l += 1
+                    return target
+                if abs(target - _sum) < abs(target - res):
+                    res = _sum
 
         return res
 
 
-def test(test_name, nums, expected):
-    res = Solution().threeSum(nums)
-    [item.sort() for item in res]
-    res.sort()
-    [item.sort() for item in expected]
-    expected.sort()
+def test(test_name, nums, target, expected):
+    slt = Solution()
+    res = slt.threeSumClosest(nums, target)
     if res == expected:
         print(test_name + ' success.')
     else:
@@ -38,13 +31,12 @@ def test(test_name, nums, expected):
 
 
 if __name__ == '__main__':
-    nums1 = [-1, 0, 1, 2, -1, -4]
-    expected1 = [
-        [-1, 0, 1],
-        [-1, -1, 2]
-    ]
-    test('test1', nums1, expected1)
+    nums1 = [-1, 2, 1, -4]
+    target1 = 1
+    expected1 = 2
+    test('test1', nums1, target1, expected1)
 
-    nums2 = [0,0]
-    expected2 = []
-    test('test2', nums2, expected2)
+    nums2 = [0,2,1,-3]
+    target2 = 1
+    expected2 = 0
+    test('test2', nums2, target2, expected2)
