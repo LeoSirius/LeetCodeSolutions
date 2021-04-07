37361799074f6dd230e81ce393eeabf4f5218bb0
LeoSirius <is.li.xiaoyu@qq.com>
2021-04-07 12:35:36 +0800

    update

--- t.py
+++ t.py
@@ -1,42 +1,57 @@
 from typing import *
 
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        res = nums[0] + nums[1] + nums[2]
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        N = len(nums)
+        pivot = N - 1
+        for i in range(N - 1, -1, -1):
+            if i == 0:
+                nums.reverse()
+                return
+            if nums[i] > nums[i-1]:
+                pivot = i
+                break
+        
+        idx = N
+        for i in range(N - 1, pivot - 1, -1):
+            if nums[i] > nums[pivot - 1]:
+                idx = i
+                break
+        
+        nums[pivot-1], nums[i] = nums[i], nums[pivot-1]
+        l, r = pivot, N - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
 
-        for i in range(len(nums) - 2):
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                _sum = nums[i] + nums[l] + nums[r]
-                if _sum > target:
-                    r -= 1
-                elif _sum < target:
-                    l += 1
-                else:
-                    return target
-                if abs(target - _sum) < abs(target - res):
-                    res = _sum
 
-        return res
-
-
-def test(test_name, nums, target, expected):
-    slt = Solution()
-    res = slt.threeSumClosest(nums, target)
-    if res == expected:
+def test(test_name, nums, expected):
+    Solution().nextPermutation(nums)
+    if nums == expected:
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
 
 
 if __name__ == '__main__':
-    nums1 = [-1, 2, 1, -4]
-    target1 = 1
-    expected1 = 2
-    test('test1', nums1, target1, expected1)
+    nums1 = [1,2,3]
+    expected1 = [1,3,2]
+    test("test1", nums1, expected1)
 
-    nums2 = [0,2,1,-3]
-    target2 = 1
-    expected2 = 0
-    test('test2', nums2, target2, expected2)
+    nums2 = [3,2,1]
+    expected2 = [1,2,3]
+    test("test2", nums2, expected2)
+
+    nums3 = [1,1,5]
+    expected3 = [1,5,1]
+    test("test3", nums3, expected3)
+
+    nums4 = [1,3,2]
+    expected4 = [2,1,3]
+    test("test4", nums4, expected4)
+
+
