6e45c50f705170ce18e377fd08996910043b3405
LeoSirius <is.li.xiaoyu@qq.com>
2021-04-23 18:25:32 +0800

    update

--- t.py
+++ t.py
@@ -3,37 +3,39 @@ from typing import *
 class Solution:
     def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
 
-        def get_sum(i, j, m, n):
-            res = 0
-
-            for x in range(i, m+1):
-                for y in range(j, n+1):
-                    res += matrix[x][y]
-            return res
-        
-
-        # for item in matrix:
-        #     item.sort()
-        # matrix.sort()
-
-        max_sum = -float('inf')
+        res = -float('inf')
         row_num, col_num = len(matrix), len(matrix[0])
-        for row in range(row_num):
-            for col in range(col_num):
-                for i in range(row+1):
-                    for j in range(col+1):
-                        currnet_sum = get_sum(i, j, row, col)
-                        if currnet_sum > k:
-                            continue
-                        max_sum = max(max_sum, currnet_sum)
-        # print(f'max_sum = {max_sum}')
-        return max_sum
+        # print(f'col_num = {col_num}')
+        dp = [[0] * (col_num+1) for _ in range(row_num+1)]
+        dp = [dp for _ in range(col_num+1)]
+        dp = [dp for _ in range(row_num+1)]
+        print(f'lendp = {len(dp)}, lendp0 = {len(dp[0])}, lendp00 = {len(dp[0][0])}, lendp000 = {len(dp[0][0][0])}')
 
-        # i, j = 0, 0
-        # while i <= m and j <= n:
-        #     current_sum = get_sum(i, j, m, n)
-        #     max_sum = max(current_sum, max_sum)
+        for i1 in range(1, row_num+1):
+            for j1 in range(1, col_num+1):
+                dp[i1][j1][i1][j1] = matrix[i1][j1]
+                if dp[i1][j1][i1][j1] <= k:
+                    res = max(res, dp[i1][j1][i1][j1])
+                for i2 in range(i1, row_num+1):
+                    for j2 in range(j1, col_num+1):
+                        dp[i1][j1][i2][j2] = dp[i1][j1][i2-1][j2] + dp[i1][j1][i2][j2 - 1] - dp[i1][j1][i2-1][j2 -1] + matrix[i2][j2]
+                        print(f'{i1} {j1} {i2} {j2} dp = {dp[i1][j1][i2][j2]}')
+                        if dp[i1][j1][i2][j2] <= k:
+                            res = max(res, dp[i1][j1][i2][j2])
+        print(f'res = {res}')
+        return res
 
+
+
+        # for row in range(row_num):
+        #     for col in range(col_num):
+        #         for i in range(row+1):
+        #             for j in range(col+1):
+        #                 currnet_sum = get_sum(i, j, row, col)
+        #                 if currnet_sum > k:
+        #                     continue
+        #                 max_sum = max(max_sum, currnet_sum)
+        # return max_sum
 
 
 def test(test_name, matrix, k, expected):
@@ -51,14 +53,22 @@ if __name__ == '__main__':
     ]
     k1 = 2
     expected1 = 2
-    test('test1', matrix1, k1, expected1)
+    # test('test1', matrix1, k1, expected1)
 
-    matrix2 = [[2,2,-1]]
-    k2 = 3
-    expected2 = 3
-    test('test2', matrix2, k2, expected2)
+    # matrix2 = [[2,2,-1]]
+    # k2 = 3
+    # expected2 = 3
+    # test('test2', matrix2, k2, expected2)
 
-    matrix3 = [[2,2,-1]]
-    k3 = 0
-    expected3 = -1
-    test('test3', matrix3, k3, expected3)
+    # matrix3 = [[2,2,-1]]
+    # k3 = 0
+    # expected3 = -1
+    # test('test3', matrix3, k3, expected3)
+
+    matrix4 = [
+        [5,-4,-3,4],
+        [-3,-4,4,5],
+        [5,1,5,-4]]
+    k4 = 10
+    expected4 = 10
+    test('test4', matrix4, k4, expected4)
