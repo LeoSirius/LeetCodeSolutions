b35a5a79e7d698117845fa6e9da57b8c56a8c386
LeoSirius <is.li.xiaoyu@qq.com>
2021-05-06 11:56:41 +0800

    update

--- t.py
+++ t.py
@@ -1,42 +1,29 @@
 from typing import *
 
+
 class Solution:
-    def deleteAndEarn(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-
-        _max = max(nums)
-        idx2cnt = [0] * (_max + 1)
-
-        for n in nums:
-            idx2cnt[n] += 1
-
-        dp = [0] * (_max+1)   # dp[0] 不用
-        dp[1] = idx2cnt[1] * 1
-        dp[2] = max(dp[1], idx2cnt[2] * 2)
-        for i in range(_max+1):
-            dp[i] = max(dp[i-1], dp[i-2] + i * idx2cnt[i])
-
-        return dp[-1]
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        arr = [first]
+        for i in range(0, len(encoded)):
+            arr.append(arr[i] ^ encoded[i])
+        return arr
 
 
-def test(test_name, nums, expected):
-    res = Solution().deleteAndEarn(nums)
+def test(test_name, encoded, first, expected):
+    res = Solution().decode(encoded, first)
     if res == expected:
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
 
 
-if __name__ == '__main__':
-    nums1 = [3,4,2]
-    expected1 = 6
-    test('test1', nums1, expected1)
+if __name__ == "__main__":
+    encoded1 = [1,2,3]
+    first1 = 1
+    expected1 = [1,0,2,1]
+    test('test1', encoded1, first1, expected1)
 
-    nums2 = [2,2,3,3,3,4]
-    expected2 = 9
-    test('test2', nums2, expected2)
-
-    nums3 = [1]
-    expected3 = 1
-    test('test3', nums3, expected3)
+    encoded2 = [6,2,7,3]
+    first2 = 4
+    expected2 = [4,2,0,7,4]
+    test('test2', encoded2, first2, expected2)
