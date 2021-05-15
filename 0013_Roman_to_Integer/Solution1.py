5658a2316a29ecbff08a364540b85d90083b8479
LeoSirius <is.li.xiaoyu@qq.com>
2021-05-15 15:23:06 +0800

    update

--- 0013_Roman_to_Integer/Solution1.py
+++ 0013_Roman_to_Integer/Solution1.py
@@ -1,15 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         mp = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
         }
         res = 0
-        for i in range(len(s) - 1):
-            if mp[s[i]] < mp[s[i + 1]]:
+        for i in range(len(s)-1):
+            if mp[s[i]] < mp[s[i+1]]:
                 res -= mp[s[i]]
             else:
                 res += mp[s[i]]
-        return res + mp[s[-1]]
+        res += mp[s[-1]]
+        return res
+
 
 def test(test_name, s, expected):
     res = Solution().romanToInt(s)
@@ -17,6 +25,7 @@ def test(test_name, s, expected):
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
+
 
 if __name__ == "__main__":
     test("test1", "MCMXCIV", 1994)
--- t.py
+++ t.py
@@ -1,21 +1,26 @@
-from typing import *
-
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 1:
-            return 0
-        _min = prices[0]
-        _max = 0
+    def romanToInt(self, s: str) -> int:
+        mp = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+        }
+        res = 0
+        for i in range(len(s)-1):
+            if mp[s[i]] < mp[s[i+1]]:
+                res -= mp[s[i]]
+            else:
+                res += mp[s[i]]
+        res += mp[s[-1]]
+        return res
 
-        for price in prices:
-            _min = min(_min, price)
-            _max = max(_max, price - _min)
 
-        return _max
-
-
-def test(test_name, prices, expected):
-    res = Solution().maxProfit(prices)
+def test(test_name, s, expected):
+    res = Solution().romanToInt(s)
     if res == expected:
         print(test_name + ' success.')
     else:
@@ -23,11 +28,4 @@ def test(test_name, prices, expected):
 
 
 if __name__ == "__main__":
-    prices1 = [7,1,5,3,6,4]
-    expected1 = 5
-    test('test1', prices1, expected1)
-
-    prices2 = [7,6,4,3,1]
-    expected2 = 0
-    test('test2', prices2, expected2)
-
+    test("test1", "MCMXCIV", 1994)
