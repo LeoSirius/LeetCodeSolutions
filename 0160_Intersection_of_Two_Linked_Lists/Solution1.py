bd89f68f60189dddc29e93bd99790294cbcd01cd
LeoSirius <is.li.xiaoyu@qq.com>
2021-06-04 12:08:55 +0800

    update

--- 0160_Intersection_of_Two_Linked_Lists/Solution1.py
+++ 0160_Intersection_of_Two_Linked_Lists/Solution1.py
@@ -10,10 +10,11 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         p1, p2 = headA, headB
         while p1 != p2:
-            # 最后一个None要走到，不然C=0时跳不出去
-            p1 = p1.next if p1 else headB  # 注意这里是让p1走B，让p2走A
+            # 最后一个 None 要走到，不然没有intersection的时候跳不出去
+            p1 = p1.next if p1 else headB
             p2 = p2.next if p2 else headA
         return p1
+
 
 def test(test_name, headA: ListNode, headB: ListNode, expected: ListNode):
     res = Solution().getIntersectionNode(headA, headB)
@@ -21,6 +22,7 @@ def test(test_name, headA: ListNode, headB: ListNode, expected: ListNode):
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
+
 
 if __name__ == "__main__":
     # 输入：intersectVal = 8,
@@ -38,11 +40,3 @@ if __name__ == "__main__":
     headB2 = build_list([1,5])
     expected2 = None
     test('test2', headA2, headB2, expected2)
-
-
-# 如果两个链表没有交点，返回 null.
-# 在返回结果后，两个链表仍须保持原有的结构。
-# 可假定整个链表结构中没有循环。
-# 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
-
-
--- t.py
+++ t.py
@@ -1,38 +1,42 @@
+from util_py.list import *
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def reverse(self, x: int) -> int:
-        INT_MAX = 2 ** 31 - 1
-        INT_MIN = -(2 ** 31)
-
-        is_neg = False
-        if x < 0:
-            is_neg = True
-            x = -x
-
-        res = 0
-        while x:
-            d = x % 10
-            x //= 10
-            if is_neg:
-                if (res > INT_MAX // 10) or (res == INT_MAX // 10 and d > 8):
-                    return 0
-            else:
-                if (res > INT_MAX // 10) or (res == INT_MAX // 10 and d > 7):
-                    return 0
-            res = res * 10 + d
-
-        return -res if is_neg else res
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        p1, p2 = headA, headB
+        while p1 != p2:
+            # 最后一个 None 要走到，不然没有intersection的时候跳不出去
+            p1 = p1.next if p1 else headB
+            p2 = p2.next if p2 else headA
+        return p1
 
 
-def test(test_name, x, expected):
-    res = Solution().reverse(x)
-    if res == expected:
+def test(test_name, headA: ListNode, headB: ListNode, expected: ListNode):
+    res = Solution().getIntersectionNode(headA, headB)
+    if is_equal_list(res, expected):
         print(test_name + ' success.')
     else:
         print(test_name + ' failed.')
 
 
 if __name__ == "__main__":
-    test('test1', 123, 321)
-    test('test2', -123, -321)
-    test('test3', 120, 21)
-    test('test4', 1534236469, 0)
+    # 输入：intersectVal = 8,
+    # listA = [4,1,8,4,5],
+    # listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+    # 输出：Reference of the node with value = 8
+    headA1 = build_list([4,1,8,4,5])
+    headB1 = build_list([5,0,1])
+    headB1.next.next.next = headA1.next.next
+    expected1 = build_list([8,4,5])
+
+    test('test1', headA1, headB1, expected1)
+
+    headA2 = build_list([2,6,4])
+    headB2 = build_list([1,5])
+    expected2 = None
+    test('test2', headA2, headB2, expected2)
