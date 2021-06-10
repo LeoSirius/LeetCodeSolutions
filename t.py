e0e2e9150a06cf03100cc58f865b71b494bac8d4
LeoSirius <is.li.xiaoyu@qq.com>
2021-06-10 19:27:06 +0800

    update

--- t.py
+++ t.py
@@ -1,27 +1,22 @@
 from typing import *
 
+
 class Solution:
-    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        MOD = 10 ** 9 + 7
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
 
-        _len = len(group)
-        dp = [[[0] * (minProfit+1) for _ in range(n+1)] for _ in range(_len+1)]
-        dp[0][0][0] = 1
-        for i in range(1, _len+1):
-            members, earn = group[i-1], profit[i-1]
-            for j in range(n+1):
-                for k in range(minProfit+1):
-                    if j < members:
-                        dp[i][j][k] = dp[i-1][j][k]
-                    else:
-                        dp[i][j][k] = (dp[i-1][j][k] + dp[i-1][j-members][max(0, k-earn)]) % MOD
-
-        total = sum(dp[_len][j][minProfit] for j in range(n+1))
-        return total % MOD
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i < coin:
+                    continue
+                dp[i] += dp[i-coin]
+        return dp[amount]
 
 
-def test(test_name, n, minProfit, group, profit, expected):
-    res = Solution().profitableSchemes(n, minProfit, group, profit)
+def test(test_name, amount, coins, expected):
+    res = Solution().change(amount, coins)
+    print(res)
     if res == expected:
         print(test_name + ' success.')
     else:
@@ -29,16 +24,22 @@ def test(test_name, n, minProfit, group, profit, expected):
 
 
 if __name__ == '__main__':
-    n1 = 5
-    minProfit1 = 3
-    group1 = [2,2]
-    profit1 = [2,3]
-    expected1 = 2
-    test('test1', n1, minProfit1, group1, profit1, expected1)
+    amount1 = 5
+    coins1 = [1,2,5]
+    expected1 = 4
+    test('test1', amount1, coins1, expected1)
 
-    n2 = 10
-    minProfit2 = 5
-    group2 = [2,3,5]
-    profit2 = [6,7,8]
-    expected2 = 7
-    test('test2', n2, minProfit2, group2, profit2, expected2)
+    amount2 = 3
+    coins2 = [2]
+    expected2 = 0
+    test('test2', amount2, coins2, expected2)
+
+    amount3 = 10
+    coins3 = [10]
+    expected3 = 1
+    test('test3', amount3, coins3, expected3)
+
+    amount4 = 500
+    coins4 = [3,5,7,8,9,10,11]
+    expected4 = 0
+    test('test4', amount4, coins4, expected4)
