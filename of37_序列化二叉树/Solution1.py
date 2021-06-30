94bbbf43ed808f9726a5d5469b49a92c38d14bf0
LeoSirius <is.li.xiaoyu@qq.com>
2021-06-30 15:48:09 +0800

    update

--- of37_序列化二叉树/Solution1.py
+++ of37_序列化二叉树/Solution1.py
@@ -1,57 +1,68 @@
-from utils_py.tree import *
+from collections import deque
+
+from util_py.tree import *
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Codec:
 
     def serialize(self, root):
         """Encodes a tree to a single string.
-        
+
         :type root: TreeNode
         :rtype: str
         """
-        res = ''
-        if not root:
-            return 'null'   # 注意这里不能返回空串
+        items = []
+        dq = deque([root])
+        while dq:
+            cur_node = dq.popleft()
+            items.append(str(cur_node.val) if cur_node else 'null')
+            if cur_node:
+                dq.append(cur_node.left)
+                dq.append(cur_node.right)
+        return ','.join(items) if items else 'null'
 
-        que = [root]
-        while que:
-            p = que.pop(0)
-            if p:
-                res += str(p.val) + ','
-                que.append(p.left)
-                que.append(p.right)
-            else:
-                res += 'null,'
-        return res[:-1]
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
-        
+
         :type data: str
         :rtype: TreeNode
         """
+        items = data.split(',')
         dummy = TreeNode(0)
-        que = [dummy]
-        left_child = False
+        dq = deque([dummy])
 
-        for val_str in data.split(','):
-            node = None if val_str == 'null' else TreeNode(int(val_str))
-            p = que[0] # 注意这里没有pop，在左右子树都接上后，再pop
-            if left_child:
-                p.left = node
+        is_left = False
+        for item in items:
+            node = None if item == 'null' else TreeNode(int(item))
+            q = dq[0]
+            if is_left:
+                q.left = node
             else:
-                p.right = node
-                que.pop(0)
-            
+                q.right = node
+                dq.popleft()
             if node:
-                que.append(node)
-            
-            left_child = not left_child
-
+                dq.append(node)
+            is_left = not is_left
         return dummy.right
+
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
 
 def test1():
     #     1
@@ -60,7 +71,7 @@ def test1():
     #      / \
     #     4   5
 
-    # 序列化为 "[1,2,3,null,null,4,5]"
+    # 序列化为 "1,2,3,null,null,4,5"
     tree = TreeNode(1)
     tree.left = TreeNode(2)
     tree.right = TreeNode(3)
@@ -69,9 +80,9 @@ def test1():
     codec = Codec()
     res_tree = codec.deserialize(codec.serialize(tree))
     if is_equal_tree(res_tree, tree):
-        print('test1 success.')
+        print('test1 succeed')
     else:
-        print('test1 failed.')
+        print('test1 fail')
 
 
 def test2():
@@ -79,9 +90,9 @@ def test2():
     codec = Codec()
     res_tree = codec.deserialize(codec.serialize(tree))
     if is_equal_tree(res_tree, tree):
-        print('test2 success.')
+        print('test2 succeed')
     else:
-        print('test2 failed.')
+        print('test2 fail')
 
 if __name__ == "__main__":
     test1()
--- t.py
+++ t.py
@@ -1,57 +1,99 @@
-from typing import *
+from collections import deque
+
+from util_py.tree import *
 
 
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        res = nums[0]
-        cnt = 1
-        for i in range(1, len(nums)):
-            if nums[i] == res:
-                cnt += 1
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        items = []
+        dq = deque([root])
+        while dq:
+            cur_node = dq.popleft()
+            items.append(str(cur_node.val) if cur_node else 'null')
+            if cur_node:
+                dq.append(cur_node.left)
+                dq.append(cur_node.right)
+        return ','.join(items) if items else 'null'
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        items = data.split(',')
+        dummy = TreeNode(0)
+        dq = deque([dummy])
+
+        is_left = False
+        for item in items:
+            node = None if item == 'null' else TreeNode(int(item))
+            q = dq[0]
+            if is_left:
+                q.left = node
             else:
-                cnt -= 1
-            if cnt <= 0:
-                cnt = 1
-                res = nums[i]
-        return res
+                q.right = node
+                dq.popleft()
+            if node:
+                dq.append(node)
+            is_left = not is_left
+        return dummy.right
 
 
-def test(test_name, nums, expected):
-    res = Solution().majorityElement(nums)
-    if res == expected:
-        print(test_name + ' success.')
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
+
+def test1():
+    #     1
+    #    / \
+    #   2   3
+    #      / \
+    #     4   5
+
+    # 序列化为 "1,2,3,null,null,4,5"
+    tree = TreeNode(1)
+    tree.left = TreeNode(2)
+    tree.right = TreeNode(3)
+    tree.right.left = TreeNode(4)
+    tree.right.right = TreeNode(5)
+    codec = Codec()
+    res_tree = codec.deserialize(codec.serialize(tree))
+    if is_equal_tree(res_tree, tree):
+        print('test1 succeed')
     else:
-        print(test_name + ' failed.')
+        print('test1 fail')
 
+
+def test2():
+    tree = None
+    codec = Codec()
+    res_tree = codec.deserialize(codec.serialize(tree))
+    if is_equal_tree(res_tree, tree):
+        print('test2 succeed')
+    else:
+        print('test2 fail')
 
 if __name__ == "__main__":
-    nums1 = [3,2,3]
-    expected1 = 3
-    test("test1", nums1, expected1)
-
-    nums2 = [2,2,1,1,1,2,2]
-    expected2 = 2
-    test("test2", nums2, expected2)
-
-    nums3 = [10,9,9,9,10]
-    expected3 = 9
-    test('test3', nums3, expected3)
-
-
-# Given an array of size n, find the majority element.
-# The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-
-# You may assume that the array is non-empty and the majority
-# element always exist in the array.
-
-# Example 1:
-
-# Input: [3,2,3]
-# Output: 3
-
-# Example 2:
-
-# Input: [2,2,1,1,1,2,2]
-# Output: 2
-
-
+    test1()
+    test2()
